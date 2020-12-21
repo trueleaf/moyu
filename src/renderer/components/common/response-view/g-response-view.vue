67269fabe5b0bfcea1cc1899f7ea25d0fef5b381
shuxiaokai <2581105856@qq.com>
2020-12-21 22:31:36 +0800

    feat: 新增返回值组件

---
+++ src/renderer/components/common/response-view/g-response-view.vue
@@ -0,0 +1,108 @@
+/*
+    创建者：shuxiaokai
+    创建时间：2020-12-21 21:53
+    模块名称：返回值展示组件
+    备注：xxxx
+*/
+<template>
+    <div class="response-view">
+        <div class="d-flex a-center">
+            <div>
+                <span>状态码：</span>
+                <template v-if="response.statusCode">
+                    <span v-show="response.statusCode >= 100 && response.statusCode < 300" class="green">{{ response.statusCode }}</span>
+                    <span v-show="response.statusCode >= 300 && response.statusCode < 400" class="orange">{{ response.statusCode }}</span>
+                    <span v-show="response.status >= 400" class="red">{{ response.statusCode }}</span>
+                </template>
+                <span v-else title="未请求数据" class="el-icon-question gray-500"></span>
+            </div>
+            <el-divider direction="vertical"></el-divider>
+            <div>
+                <span>时长：</span>
+                <template v-if="response.rt">
+                    <span v-show="response.rt >= 0 && response.rt < 2000" class="green">{{ formatMs }}</span>
+                    <span v-show="response.rt >= 2000 && response.rt < 5000" class="orange">{{ formatMs }}</span>
+                    <span v-show="response.status >= 5000" class="red">{{ formatMs }}</span>
+                </template>
+                <span v-else title="未请求数据" class="el-icon-question gray-500"></span>
+            </div>
+            <el-divider direction="vertical"></el-divider>
+            <div>
+                <span>大小：</span>
+                <template v-if="response.size">
+                    <span v-show="response.size >= 0 && response.size < 10000" class="green">{{ formatBytes }}</span>
+                    <span v-show="response.size >= 10000 && response.size < 15000" class="orange">{{ formatBytes }}</span>
+                    <span v-show="response.status >= 15000" class="red">{{ formatBytes }}</span>
+                </template>
+                <span v-else title="未请求数据" class="el-icon-question gray-500"></span>
+            </div>
+            <el-divider direction="vertical"></el-divider>
+            <div class="d-flex a-center j-center">
+                <span>格式：</span>
+                <span v-if="response.mime">{{ response.mime }}</span>
+                <span v-else title="未请求数据" class="el-icon-question gray-500"></span>
+            </div>
+        </div>
+        <div>
+            <div v-if="response.mime === 'image/svg+xml'" v-html="response.value"></div>
+            <s-json v-if="response.mime.includes('application/json')" :data="JSON.parse(response.value)"></s-json>
+            <div v-if="response.mime === 'image/svg+xml'" v-html="response.value"></div>
+            <div v-if="response.mime === 'image/svg+xml'" v-html="response.value"></div>
+            <div v-if="response.mime === 'image/svg+xml'" v-html="response.value"></div>
+            <div v-if="response.mime === 'image/svg+xml'" v-html="response.value"></div>
+            <div v-if="response.mime === 'image/svg+xml'" v-html="response.value"></div>
+            <div v-if="response.mime === 'image/svg+xml'" v-html="response.value"></div>
+            <div v-if="response.mime === 'image/svg+xml'" v-html="response.value"></div>
+            <div v-if="response.mime === 'image/svg+xml'" v-html="response.value"></div>
+            <div v-if="response.mime === 'image/svg+xml'" v-html="response.value"></div>
+            <div v-if="response.mime === 'image/svg+xml'" v-html="response.value"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        response: {
+            type: Object,
+            default() {
+                return {};
+            }
+        }
+    },
+    computed: {
+        formatBytes() {
+            return this.$helper.formatBytes(this.response.size);
+        },
+        formatMs() {
+            return this.$helper.formatMs(this.response.rt);
+        }
+    },
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    methods: {
+        //=====================================获取远程数据==================================//
+
+        //=====================================前后端交互====================================//
+
+        //=====================================组件间交互====================================//  
+        
+        //=====================================其他操作=====================================//
+
+    }
+};
+</script>
+
+
+
+<style lang="scss">
+.response-view {
+    
+}
+</style>
