b81310252ff58f7fe52c97a47c069d0fe0cd55bc
shuxiaokai <2581105856@qq.com>
2021-02-05 18:00:49 +0800

    feat: 更新eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,5 +33,15 @@ module.exports = {
         "no-param-reassign": ["error", { props: false }], //函数参数非对象情况不允许直接改变
         "linebreak-style": ["off", "windows"],
         "spaced-comment": "off", //注释不做限制
+        "prefer-destructuring": ["error", { //申明变量解构有限，赋值可以不必解构
+            "VariableDeclarator": {
+              "array": false,
+              "object": true
+            },
+            "AssignmentExpression": {
+              "array": false,
+              "object": true
+            }
+        }]
     },
 };
