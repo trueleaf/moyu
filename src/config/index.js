2f9f2f9cad595ec9784ec109003a8a770d753dc7
shuxiaokai <2581105856@qq.com>
2021-02-18 18:10:35 +0800

    feat: 更新线上配置信息

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,3 @@
-/* eslint-disable import/no-unresolved */
 const config = require("./config.default");
 
-let localConfig = null;
-try {
-    // eslint-disable-next-line global-require
-    localConfig = require("./config.local2.js")
-} catch (error) {
-    console.error(error)
-}
-console.log(localConfig)
 module.exports = config;
