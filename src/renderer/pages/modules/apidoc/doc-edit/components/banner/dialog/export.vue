b6a5262ae899c3e5c803fe0c302c5536a5f05e06
shuxiaokai <2581105856@qq.com>
2021-02-28 22:19:07 +0800

    feat: 新增生成在线链接静态页面

--- src/renderer/pages/modules/apidoc/doc-edit/components/banner/dialog/export.vue
+++ src/renderer/pages/modules/apidoc/doc-edit/components/banner/dialog/export.vue
@@ -5,19 +5,29 @@
     备注：xxxx
 */
 <template>
-    <s-dialog title="导出文档" :isShow="visible" width="40%" class="doc-export" @close="handleClose">
-        <div class="download-wrap">
-            <s-download url="/api/project/doc_offline_data" :params="{ projectId: $route.query.id }" class="item">
-                <svg class="svg-icon" aria-hidden="true">
-                    <use xlink:href="#iconhtml"></use>
-                </svg>
-                <div class="mt-1">HTML</div>
-            </s-download>
-            <s-download url="/api/project/export/moyu" :params="{ projectId: $route.query.id }" class="item">
-                <img src="@/assets/imgs/logo.png" alt="moyu" class="svg-icon">
-                <div class="mt-1">JSON文档</div>
-            </s-download>
-        </div>
+    <s-dialog :isShow="visible" width="40%" class="doc-export" @close="handleClose">
+        <s-fieldset title="导出文档">
+            <div class="download-wrap">
+                <s-download url="/api/project/doc_offline_data" :params="{ projectId: $route.query.id }" class="item">
+                    <svg class="svg-icon" aria-hidden="true">
+                        <use xlink:href="#iconhtml"></use>
+                    </svg>
+                    <div class="mt-1">HTML</div>
+                </s-download>
+                <s-download url="/api/project/export/moyu" :params="{ projectId: $route.query.id }" class="item">
+                    <img src="@/assets/imgs/logo.png" alt="moyu" class="svg-icon">
+                    <div class="mt-1">JSON文档</div>
+                </s-download>
+            </div>
+        </s-fieldset>
+        <s-fieldset title="生成在线链接">
+            <div class="d-flex">
+                <pre class="w-80"></pre>
+                <el-button size="mini">生成</el-button>
+                <el-button size="mini">复制</el-button>
+                <el-button size="mini">高级</el-button>
+            </div>
+        </s-fieldset>
         <div slot="footer">
             <el-button size="mini" type="warning" @click="handleClose">关闭</el-button>
         </div>
@@ -76,7 +86,7 @@ export default {
             justify-content: center;
             flex-direction: column;
             &:hover {
-                box-shadow: $box-shadow-sm;
+                border: 1px solid $gray-400;
             }
             .svg-icon {
                 width: size(70);
