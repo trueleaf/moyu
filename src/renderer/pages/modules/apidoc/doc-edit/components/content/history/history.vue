30e83e2b1873bd30664c24c283439762d90cddf9
shuxiaokai <2581105856@qq.com>
2021-03-19 10:01:41 +0800

    feat: 更新历史记录模块

--- src/renderer/pages/modules/apidoc/doc-edit/components/content/history/history.vue
+++ src/renderer/pages/modules/apidoc/doc-edit/components/content/history/history.vue
@@ -5,8 +5,29 @@
     备注：
 */
 <template>
-    <div>
-        ss
+    <div class="history">
+        <div class="search-wrap">
+            <div class="d-flex">
+                <div>操作人员：</div>
+                <el-checkbox-group v-model="formInfo.operators">
+                    <el-checkbox label="小明"></el-checkbox>
+                    <el-checkbox label="小李"></el-checkbox>
+                    <el-checkbox label="小花"></el-checkbox>
+                </el-checkbox-group>
+            </div>
+            <div class="d-flex">
+                <div>日期范围：</div>
+                <el-checkbox-group v-model="formInfo.operators">
+                    <el-checkbox label="今天之内"></el-checkbox>
+                    <el-checkbox label="24小时内"></el-checkbox>
+                    <el-checkbox label="3天内"></el-checkbox>
+                    <el-checkbox label="7天内"></el-checkbox>
+                </el-checkbox-group>
+            </div>
+        </div>
+        <pre>
+            {{ historyList }}
+        </pre>
     </div>
 </template>
 
@@ -15,20 +36,40 @@ export default {
     data() {
         return {
             //=================================表单与表格参数================================//
-
+            formInfo: {
+                operators: [],
+            },
+            historyList: [], //历史记录列表
             //===================================枚举参数====================================//
 
             //===================================业务参数====================================//
 
             //===================================其他参数====================================//
+            loading: false, //是否正在请求数据
         };
     },
     created() {
-
+        this.getData();
+        this.$event.on("apidoc/fresh", () => {
+            this.getData()
+        });
     },
     methods: {
         //==================================初始化&获取远端数据===============================//
-
+        //获取数据
+        getData() {
+            this.loading = true;
+            const params = {
+                projectId: this.$route.query.id,
+            };
+            this.axios.get("/api/docs/docs_history", { params }).then((res) => {
+                this.historyList = res.data.rows;
+            }).catch((err) => {
+                console.error(err);
+            }).finally(() => {
+                this.loading = false;
+            });
+        },
         //=====================================前后端交互====================================//
 
         //=====================================组件间交互====================================//
