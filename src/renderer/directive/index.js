133221281a4319a1707ae038436edc6ab5455958
shuxiaokai <2581105856@qq.com>
2021-03-22 18:00:27 +0800

    feat: 新增自动计算高度指令

--- src/renderer/directive/index.js
+++ src/renderer/directive/index.js
@@ -3,9 +3,11 @@ import Vue from "vue";
 import copy from "./copy";
 import contextCopy from "./context-copy";
 import successTip from "./success";
+import flex1 from "./flex1";
 
 export default (() => {
     Vue.directive("copy", copy);
     Vue.directive("copy2", contextCopy);
     Vue.directive("success", successTip);
+    Vue.directive("flex1", flex1);
 })();
