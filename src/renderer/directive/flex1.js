7d6688144d26d24e349aeabc7495e2ac804e1625
shuxiaokai <2581105856@qq.com>
2021-03-22 18:00:17 +0800

    feat: 新增自动计算高度指令

---
+++ src/renderer/directive/flex1.js
@@ -0,0 +1,13 @@
+/**
+ * @description        自动计算高度适配屏幕
+ * @author             shuxiaokai
+ * @create             2021-03-22 22:44
+ */
+
+export default {
+    update(el, binding) {
+        const offsetY = el.getBoundingClientRect().y;
+        const { value } = binding;
+        el.style.height = `calc(100vh - ${offsetY + value}px)`;
+    },
+};
