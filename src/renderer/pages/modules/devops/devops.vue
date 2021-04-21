da298f74ecd417eda3baf419e2ad7535a561ec56
shuxiaokai <2581105856@qq.com>
2021-04-21 18:40:14 +0800

    feat: 新增远程服务器连接

--- src/renderer/pages/modules/devops/devops.vue
+++ src/renderer/pages/modules/devops/devops.vue
@@ -5,7 +5,7 @@
     备注：
 */
 <template>
-    <div>
+    <div class="w-100 h-100">
         <s-fieldset title="认证相关" class="w-50">
             <el-form ref="form" :model="formInfo" label-width="120px">
                 <el-form-item label="项目名称">
@@ -26,12 +26,21 @@
             </el-form>
             <div class="d-flex j-center">
                 <el-button :loading="loading" :size="config.renderConfig.layout.size" type="success" @click="handleConnect">连接</el-button>
+                <el-button :loading="loading" :size="config.renderConfig.layout.size" type="success" @click="handleDisconnect">断开连接</el-button>
             </div>
+            <div class="d-flex">
+                <el-input v-model="command"></el-input>
+                <el-button :size="config.renderConfig.layout.size" @click="handleExec">执行命令</el-button>
+            </div>
+            <pre>{{ commandResult }}</pre>
         </s-fieldset>
+        <div ref="terminal"></div>
     </div>
 </template>
 
 <script>
+import { Terminal } from "xterm";
+import "xterm/css/xterm.css"
 import Ssh2 from "./ssh2"
 
 let shell = null;
@@ -45,34 +54,94 @@ export default {
             formInfo: {
                 projectName: "",
                 username: "dev",
-                password: "GfrrWOJTYF",
+                password: "",
                 host: "10.31.45.148",
                 port: 22,
             },
             //===================================枚举参数====================================//
 
             //===================================业务参数====================================//
+            ssh2Instance: null,
             clientInstance: null,
+            shellStream: null,
+            command: "",
+            commandResult: "",
+            terminal: null,
             //===================================其他参数====================================//
             loading: false,
         };
     },
-    created() {
+    mounted() {
         console.log(shell)
         this.initSsh();
+        this.initTerminal();
     },
     methods: {
         //==================================初始化&获取远端数据===============================//
+        initTerminal() {
+            this.terminal = new Terminal();
+            this.terminal.open(this.$refs.terminal);
+            this.terminal.onData((val) => {
+                this.shellStream.write(val);
+            });
+            // terminal.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ")
+        },
         //初始化ssh
         initSsh() {
-            this.clientInstance = new Ssh2();
+            this.ssh2Instance = new Ssh2();
         },
         //ssh连接
         handleConnect() {
-            this.clientInstance.connect(this.formInfo);
+            this.loading = true;
+            this.ssh2Instance.connect(this.formInfo).then((sshInstance) => {
+                this.clientInstance = sshInstance;
+                sshInstance.on("end", () => {
+                    this.handleEndSocket();
+                });
+                sshInstance.on("close", () => {
+                    this.handleCloseSocket();
+                });
+                this.initShell();
+            }).catch((err) => {
+                this.$message.error(err.message);
+            }).finally(() => {
+                this.loading = false;
+            });
         },
-        //=====================================前后端交互====================================//
-
+        //主动断开ssh连接
+        handleDisconnect() {
+            this.ssh2Instance.disconnect();
+        },
+        initShell() {
+            this.clientInstance.shell({}, (err, stream) => {
+                if (err) {
+                    console.error(err);
+                }
+                this.shellStream = stream;
+                stream.on("data", (data) => {
+                    console.log("shell", data.toString("utf-8"))
+                    this.terminal.write(data);
+                })
+                stream.on("close", () => {
+                    console.log("流关闭")
+                })
+            })
+        },
+        //=====================================执行命令====================================//
+        async handleExec() {
+            try {
+                this.commandResult = await this.ssh2Instance.exec(this.command);
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        //=====================================socket事件====================================//
+        //断开socket连接
+        handleEndSocket() {
+            this.$message.error("断开socket连接");
+        },
+        //关闭socket连接
+        handleCloseSocket() {},
         //=====================================组件间交互====================================//
 
         //=====================================其他操作=====================================//
--- src/renderer/pages/modules/devops/ssh2.js
+++ src/renderer/pages/modules/devops/ssh2.js
@@ -26,26 +26,84 @@ class Ssh2 {
      * @return {Ssh2}      返回实例
      */
     connect(config) {
-        const { username, password, host, port, timeout = 8000 } = config;
-        this.clientInstance = new Client();
-        this.clientInstance.connect({
-            host,
-            port: parseInt(port, 10),
-            username,
-            password,
-            readyTimeout: timeout,
+        return new Promise((resolve, reject) => {
+            const { username, password, host, port, timeout = 8000 } = config;
+            this.clientInstance = new Client();
+            this.clientInstance.connect({
+                host,
+                port: parseInt(port, 10),
+                username,
+                password,
+                readyTimeout: timeout,
+            });
+            this.clientInstance.on("ready", () => {
+                resolve(this.clientInstance);
+            })
+            this.clientInstance.on("error", (err) => {
+                console.error(err);
+                reject(err);
+            })
         });
-        this.clientInstance.on("ready", () => {
-            console.log("连接成功");
+    }
+
+    /**
+     * @description        断开连接
+     * @author             shuxiaokai
+     * @create             2021-04-21 09:23
+     * @return {String}    返回字符串
+     */
+    disconnect() {
+        this.clientInstance.end();
+    }
+
+    /**
+     * @description        执行远程命令
+     * @author             shuxiaokai
+     * @create             2021-04-21 09:58
+     * @param {string}     command - 远程命令
+     * @param {Object?}    options - 配置参数
+     * @return {Promise}   返回Promise
+     */
+    exec(command, options = {}) {
+        return new Promise((resolve, reject) => {
+            this.clientInstance.exec(command, options, (err, stream) => {
+                if (err) {
+                    reject(err);
+                }
+                stream.on("data", (data) => {
+                    resolve(data);
+                })
+                stream.on("close", () => {
+                    console.log("流关闭")
+                })
+            });
         })
-        this.clientInstance.on("error", (err) => {
-            console.error(err);
-        })
-        this.clientInstance.on("end", () => {
-            console.log("end");
-        })
-        this.clientInstance.on("close", () => {
-            console.log("close");
+    }
+
+    /**
+     * @description        执行shell命令
+     * @author             shuxiaokai
+     * @create             2021-04-21 13:34
+     * @return {Promise}   返回Promise
+     */
+    initShell() {
+        return new Promise((resolve, reject) => {
+            this.clientInstance.shell({}, (err, stream) => {
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                }
+                stream.on("data", (data) => {
+                    console.log("shell", data.toString("utf-8"))
+                    resolve(data);
+                })
+                stream.on("close", () => {
+                    console.log("流关闭")
+                })
+                stream.write("ls -l\n", "utf-8")
+                stream.write("cd /var/data\n", "utf-8")
+                stream.write("ls -l\n", "utf-8")
+            })
         })
     }
 }
