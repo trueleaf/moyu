36df21e47424e5fadc63b66807d75f830ac9a5b8
shuxiaokai <2581105856@qq.com>
2021-05-24 18:20:53 +0800

    feat: 更新已删除列表详情展示

--- src/renderer/pages/modules/apidoc/mixin/content/recycler/components/doc-detail.vue
+++ src/renderer/pages/modules/apidoc/mixin/content/recycler/components/doc-detail.vue
@@ -7,14 +7,40 @@
 <template>
     <s-loading :loading="loading" class="doc-detail">
         <div class="close el-icon-close" @click="handleClose"></div>
-        <div v-if="apidocItem.url" class="d-flex a-center mt-5 px-2 mb-1 border-bottom-gray-400">
-            <template v-for="(req) in validRequestMethods">
-                <span v-if="apidocItem.method === req.value.toLowerCase()" :key="req.value" class="mr-1" :style="{color: req.iconColor}">{{ req.name }}</span>
-            </template>
-            <div class="flex1 f-base">{{ apidocItem.url.path }}</div>
-        </div>
         <div class="params-view">
-            <s-fieldset title="请求参数" class="mb-5">
+            <s-fieldset v-if="apidocItem.url" title="基本信息">
+                <s-label-value v-if="!isFolder" label="请求方式：" class="w-50">
+                    <template v-for="(req) in validRequestMethods">
+                        <span v-if="apidocItem.method === req.value.toLowerCase()" :key="req.name" class="label" :style="{color: req.iconColor}">{{ req.name.toUpperCase() }}</span>
+                    </template>
+                </s-label-value>
+                <s-label-value v-if="!isFolder" label="请求地址：" class="w-50 mt-2">
+                    <span class="text-ellipsis">{{ apidocItem.url.path }}</span>
+                </s-label-value>
+                <s-label-value label="接口名称：" class="w-50">
+                    <div>{{ apidocInfo.name }}</div>
+                </s-label-value>
+                <s-label-value label="标签信息：" class="w-50">
+                    <div v-if="apidocInfo.tag && apidocInfo.tag.name">
+                        <span :style="{color: apidocInfo.tag.color}">{{ apidocInfo.tag.name }}</span>
+                    </div>
+                </s-label-value>
+                <div v-if="apidocInfo" class="base-info">
+                    <s-label-value label="维护人员：" :title="apidocInfo.maintainer || apidocInfo.creator" label-width="auto" class="w-50">
+                        <span class="text-ellipsis">{{ apidocInfo.maintainer || apidocInfo.creator }}</span>
+                    </s-label-value>
+                    <s-label-value label="创建人员：" :title="apidocInfo.maintainer || apidocInfo.creator" label-width="auto" class="w-50">
+                        <span class="text-ellipsis">{{ apidocInfo.maintainer || apidocInfo.creator }}</span>
+                    </s-label-value>
+                    <s-label-value label="更新日期：" :title="formatDate(apidocInfo.updatedAt)" label-width="auto" class="w-50">
+                        <span class="text-ellipsis">{{ formatDate(apidocInfo.updatedAt) }}</span>
+                    </s-label-value>
+                    <s-label-value label="创建日期：" :title="formatDate(apidocInfo.createdAt)" label-width="auto" class="w-50">
+                        <span class="text-ellipsis">{{ formatDate(apidocInfo.createdAt) }}</span>
+                    </s-label-value>
+                </div>
+            </s-fieldset>
+            <s-fieldset v-if="!isFolder" title="请求参数" class="mb-5">
                 <template v-if="hasQueryParams || hasBodyParams || hasPathParams">
                     <s-collapse v-if="hasPathParams" title="请求参数(Path)">
                         <s-array-view :data="apidocItem.paths" show-checkbox class="mt-2"></s-array-view>
@@ -36,7 +62,7 @@
                 </template>
                 <div v-else>空</div>
             </s-fieldset>
-            <s-fieldset title="返回参数">
+            <s-fieldset v-if="!isFolder" title="返回参数">
                 <div v-for="(item, index) in apidocItem.responseParams" :key="index">
                     <s-collapse :key="index" :active="index === 0" :title="item.title">
                         <s-array-view v-if="item.values.length >= 1" :data="item.values" class="mt-2">
@@ -46,7 +72,7 @@
                     </s-collapse>
                 </div>
             </s-fieldset>
-            <s-fieldset title="请求头">
+            <s-fieldset v-if="!isFolder" title="请求头">
                 <s-array-view v-if="apidocItem.headers && apidocItem.headers.length > 1" :data="apidocItem.headers">
                     <div slot="header" v-copy="jsonHeaders" class="copy-json">复制为json</div>
                 </s-array-view>
@@ -71,6 +97,8 @@ export default {
     data() {
         return {
             apidocItem: {},
+            apidocInfo: {},
+            isFolder: false, //是否为文件夹
             loading: false,
         };
     },
@@ -137,8 +165,9 @@ export default {
                 projectId: this.$route.query.id,
             };
             this.axios.get("/api/project/doc_detail", { params }).then((res) => {
-                console.log(res.data)
                 this.apidocItem = res.data.item
+                this.apidocInfo = res.data.info;
+                this.isFolder = res.data.info.type === "folder";
             }).catch((err) => {
                 console.error(err);
             }).finally(() => {
@@ -164,9 +193,10 @@ export default {
     overflow: hidden;
     position: relative;
     .params-view {
-        height: 70vh;
+        max-height: 65vh;
         overflow-y: auto;
         padding: 0 size(10);
+        margin-top: size(30);
         .copy-json {
             cursor: pointer;
             &:hover {
