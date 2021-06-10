b41db17d88c906411de43d1746c8a82ccf79eb6c
shuxiaokai <2581105856@qq.com>
2021-06-10 18:15:29 +0800

    feat: 添加对store和axios类型声明

--- src/renderer/shims-vue.d.ts
+++ src/renderer/shims-vue.d.ts
@@ -1,4 +1,17 @@
 /* eslint-disable */
+import { ComponentCustomProperties } from "vue"
+import { Store } from "vuex"
+import { AxiosInstance } from "axios"
+
+declare module "@vue/runtime-core" {
+    interface State {
+        count: number
+    }
+    interface ComponentCustomProperties {
+        $store: Store<State>,
+        axios: AxiosInstance,
+    }
+}
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
