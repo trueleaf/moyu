602be55aec8377dd2cdf906512c9e8038c2157f0
shuxiaokai <2581105856@qq.com>
2021-06-23 18:00:51 +0800

    feat: 新增表格组件

--- src/renderer/components/common/table/g-table.vue
+++ src/renderer/components/common/table/g-table.vue
@@ -5,18 +5,284 @@
     备注：
 */
 <template>
-    <div>aa</div>
+    <s-loading :loading="loading">
+        <el-table
+            ref="table"
+            v-bind="$attrs"
+            :data="tableInfo"
+            stripe
+            border
+            :size="config.renderConfig.layout.size"
+            :height="tableHeight"
+            @selection-change="handleSelectionChange"
+        >
+            <el-table-column v-if="selection || deleteMany" type="selection" align="center" :reserve-selection="reserveSelection"></el-table-column>
+            <el-table-column v-if="index" type="index" label="序号" align="center"></el-table-column>
+            <slot />
+        </el-table>
+        <div v-if="plain !== true" class="d-flex j-end mt-1">
+            <slot name="operation" />
+            <el-button :loading="loading" type="primary" icon="el-icon-refresh" :size="config.renderConfig.layout.size" @click="getData">刷新</el-button>
+            <el-button
+                v-if="deleteMany"
+                :loading="loading2"
+                :disabled="selectData.length === 0"
+                :title="disableTip"
+                type="danger"
+                icon="el-icon-delete"
+                :size="config.renderConfig.layout.size"
+                @click="deleteData"
+            >
+                批量删除
+            </el-button>
+            <el-pagination
+                v-model:currentPage="formInfo.pageNum"
+                class="ml-4"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="total"
+                background
+                :page-sizes="config.renderConfig.components.tableConfig.pageSizes"
+                :page-size="config.renderConfig.components.tableConfig.pageSize"
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+            >
+            </el-pagination>
+        </div>
+    </s-loading>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
+import config from "@/../config/config"
 
 export default defineComponent({
+    props: {
+        /**
+         * 数据回调钩子,回调第一个参数是返回值,返回子组件
+         */
+        resHook: {
+            type: Function,
+            default: null
+        },
+        /**
+         * 请求url
+         */
+        url: {
+            type: String,
+            required: true,
+        },
+        /**
+         * 是否显示序号
+         */
+        index: {
+            type: Boolean,
+            default: true
+        },
+        /**
+         * 是否开启多选
+         */
+        selection: {
+            type: Boolean,
+            default: false
+        },
+        /**
+         * 额外参数
+         */
+        params: {
+            type: Object,
+            default: () => ({}),
+        },
+        /**
+         * 是否立即请求数据
+         */
+        immediate: {
+            type: Boolean,
+            default: true
+        },
+        /**
+         * 表格高度
+         */
+        height: {
+            type: Number,
+            default: 0
+        },
+        /**
+         * 是否存在批量删除
+         */
+        deleteMany: {
+            type: Boolean,
+            default: false,
+        },
+        /**
+         * 批量删除地址
+         */
+        deleteUrl: {
+            type: String,
+            default: ""
+        },
+        /**
+         * 批量删除key值
+         */
+        deleteKey: {
+            type: String,
+            default: "ids"
+        },
+        /**
+         * 表格数据的key值
+         */
+        deleteDataKey: {
+            type: String,
+            default: "_id"
+        },
+        /**
+         * 批量删除额外参数
+         */
+        deleteParams: {
+            type: Object,
+            default: () => ({}),
+        },
+        /**
+         * 是否记住选中项目,翻页记忆
+         */
+        reserveSelection: {
+            type: Boolean,
+            default: false
+        },
+        /**
+         * 是否展示分页
+         */
+        plain: {
+            type: Boolean,
+            default: false
+        }
+    },
+    emits: ["finish", "select", "deleteMany"],
     data() {
         return {
+            //=====================================表格参数====================================//
+            formInfo: {
+                pageSize: config.renderConfig.components.tableConfig.pageSize, //----分页大小
+                pageNum: 1, //-------------------------------------------------------当前页数
+            },
+            tableInfo: [], //--------------------------------------------------------表格数据
+            tableHeight: "100", //-----------------------------------------------------表格高度
+            selectData: [] as Record<string, unknown>[], //-------------------------------------------------------选中的表格数据
+            total: 0, //-------------------------------------------------------------数据总数
+            responseData: null, //---------------------------------------------------表格返回数据
+            //=====================================其他参数====================================//
+            loading: false, //-------------------------------------------------------获取数据加载效果
+            loading2: false, //------------------------------------------------------批量删除加载效果
+            config,
         };
     },
+    computed: {
+        disableDelete(): boolean { //是否禁止删除
+            return this.selectData.length === 0;
+        },
+        disableTip(): string { //禁止提示信息
+            if (this.selectData.length === 0) {
+                return "在左侧进行数据选择后方可删除数据";
+            } else {
+                return ""
+            }
+        }
+    },
+    mounted() {
+        this.init();
+    },
     methods: {
+        //初始化
+        init() {
+            if (this.immediate) {
+                this.getData();
+            }
+            this.initTableHeight();
+            window.addEventListener("resize", this.$helper.debounce(() => {
+                console.log(222)
+                this.initTableHeight();
+            }, 300))
+        },
+        //获取数据
+        getData(searchParams?: unknown) {
+            this.$nextTick(() => {
+                let p = {};
+                if (Object.prototype.toString.call(searchParams).slice(8, -1) !== "MouseEvent") { //修复鼠标事件导致第一个参数数据错误
+                    p = JSON.parse(JSON.stringify(searchParams || {})); //防止数据变化产生递归
+                }
+                const params = !this.plain ? Object.assign(this.formInfo, p, this.params) : Object.assign(p, this.params);
+
+                this.loading = true;
+                this.axios.get(this.url, { params }).then((res) => {
+                    this.responseData = res.data;
+                    if (this.resHook) {
+                        this.resHook(res, this);
+                    } else {
+                        this.total = res.data.total;
+                        this.tableInfo = res.data.rows;
+                    }
+                }).catch((err) => {
+                    console.error(err);
+                }).finally(() => {
+                    this.loading = false;
+                    this.$nextTick(() => {
+                        this.$emit("finish", this.responseData, this);
+                    })
+                });
+            })
+        },
+        // 分页
+        handleSizeChange(size: number) {
+            this.formInfo.pageNum = 1;
+            this.formInfo.pageSize = size;
+            this.getData();
+        },
+        // 改变页码
+        handleCurrentChange(page: number) {
+            this.formInfo.pageNum = page;
+            this.getData();
+        },
+        //=========================================================================//
+        //初始化
+        initTableHeight() {
+            const tableDom: HTMLElement = (this.$refs.table as { $el: HTMLElement }).$el;
+            //hack
+            setTimeout(() => {
+                const { top } = tableDom.getBoundingClientRect(); //表格距离顶部距离
+                const height = this.height || window.innerHeight - top - 70; //100是试出来
+                this.tableHeight = height < 200 ? "200px" : `${height}px`; //高度至少200px
+            })
+        },
+        //选择了数据
+        handleSelectionChange(val: Record<string, unknown>[]) {
+            this.selectData = val;
+            this.$emit("select", val);
+        },
+        //批量删除
+        deleteData() {
+            this.$confirm(`此操作将删除${this.selectData.length}条记录, 是否继续?`, "提示", {
+                confirmButtonText: "确定",
+                cancelButtonText: "取消",
+                type: "warning"
+            }).then(() => {
+                const params = {} as Record<string, unknown>;
+                params[this.deleteKey] = this.selectData.map((val) => val[this.deleteDataKey]);
+                Object.assign(params, this.deleteParams);
+                this.loading2 = true;
+                this.axios.delete(this.deleteUrl, { data: params }).then(() => {
+                    this.getData();
+                    this.$emit("deleteMany");
+                }).catch((err) => {
+                    console.error(err);
+                }).finally(() => {
+                    this.loading2 = false;
+                });
+            }).catch((err: Error | string) => {
+                if (err === "cancel" || err === "close") {
+                    return;
+                }
+                console.error(err);
+            });
+        },
     },
 })
 </script>
