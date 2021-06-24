bdcee3d6ca73f14403be7e0236d24d7eae4a5ddb
shuxiaokai <2581105856@qq.com>
2021-06-24 18:00:43 +0800

    feat: 更新表单组件

---
+++ src/renderer/components/common/forms/form/g-form-item.vue
@@ -0,0 +1,117 @@
+/*
+    创建者：shuxiaokai
+    创建时间：2021-06-15 22:49
+    模块名称：表单数据项
+    备注：
+*/
+<template>
+    <!-- 普通输入框 -->
+    <s-col v-if="type === 'input'" v-bind="$attrs">
+        <el-form-item :label="realLabel" :prop="prop">
+            <s-input v-model:value="formInfo[prop]" :placeholder="realPlaceholder"></s-input>
+        </el-form-item>
+    </s-col>
+    <!-- 下拉搜索框 -->
+    <s-col v-if="type === 'select'" v-bind="$attrs">
+        <el-form-item :label="realLabel" :prop="prop">
+            <s-select v-model:value="formInfo[prop]" v-bind="$attrs" :placeholder="realPlaceholder"></s-select>
+        </el-form-item>
+    </s-col>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: "FormItem",
+    inject: ["formInfo"],
+    props: {
+        /**
+         * 表单组件类型 input select date daterange text
+         */
+        type: {
+            type: String,
+            default: "input",
+        },
+        /**
+         * 文案
+         */
+        label: {
+            type: String,
+            default: ""
+        },
+        /**
+         * placeholder
+         */
+        placeholder: {
+            type: String,
+            default: "",
+        },
+        /**
+         * 绑定参数的字段名称
+         */
+        prop: {
+            type: [String, Number, Boolean, Array],
+            default: "",
+        },
+        //=====================================快捷规则====================================//
+        /**
+         * 必填校验
+         */
+        required: {
+            type: Boolean,
+            default: false,
+        },
+        /**
+         * 最大长度
+         */
+        maxLength: {
+            type: Number,
+            default: null,
+        },
+        /**
+         * 最小长度
+         */
+        minLength: {
+            type: Number,
+            default: null,
+        },
+        /**
+         * 长度恰好等于
+         */
+        length: {
+            type: Number,
+            default: null,
+        },
+        /**
+         * 手机号校验
+         */
+        phone: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    data() {
+        return {};
+    },
+    computed: {
+        realLabel(): string { //实际label值，自动拼接
+            if (this.label.endsWith("：")) {
+                return this.label;
+            } if (this.label.endsWith(":")) {
+                return this.label.replace(":", "：");
+            }
+            return `${this.label}：`;
+        },
+        realPlaceholder(): string { //实际placeholder值
+            return this.placeholder ? this.placeholder : `请输入${this.label}`;
+        },
+    },
+    methods: {
+    },
+})
+</script>
+
+<style lang="scss">
+
+</style>
--- src/renderer/components/common/forms/form/g-form.vue
+++ src/renderer/components/common/forms/form/g-form.vue
@@ -5,7 +5,7 @@
     备注：
 */
 <template>
-    <el-form ref="form" v-bind="$attrs" :model="formInfo" :rules="rules" @submit.prevent="handleSubmit">
+    <el-form ref="form" v-bind="$attrs" :model="formInfo" :label-width="labelWidth" :rules="rules" @submit.prevent="handleSubmit">
         <div v-if="config.isDev && showTips">
             {{ formInfo }}
         </div>
@@ -22,15 +22,21 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { defineComponent, VNode } from "vue"
+import { ElForm } from "@@/elementui"
 import config from "@/../config/config"
 
 export default defineComponent({
+    provide() {
+        return {
+            formInfo: this.formInfo, //将formInfo注入到item中
+        };
+    },
     props: {
         /**
          * 表单数据
          */
-        formInfo: {
+        editData: {
             type: Object,
             default: () => ({})
         },
@@ -51,11 +57,161 @@ export default defineComponent({
     },
     data() {
         return {
-            rules: {}, //-----验证规则
-            config, //--------配置信息
+            formInfo: {} as Record<string, unknown>, //---搜索参数
+            rules: {
+                // loginName: [{ required: true, message: "xxx", trigger: "blur" }],
+            } as Record<string, unknown[]>, //----验证规则
+            config, //------------------------------------配置信息
+            labelWidth: "100px", //-----------------------表单label宽度
         };
     },
+    watch: {
+        editData: {
+            handler(data) {
+                Object.keys(data).forEach((key) => {
+                    this.formInfo[key] = data[key]
+                });
+            },
+            deep: true,
+            immediate: true,
+        },
+    },
+    mounted() {
+        this.initLabelWidth(); //初始化label的宽度
+        this.initFormData(); //初始化表单数据绑定
+        this.initRules(); //初始化规则校验
+    },
     methods: {
+        //初始化label的宽度
+        initLabelWidth() {
+            const formItems: VNode[] = [];
+            if (this.$slots.default) {
+                const allSlots = this.$slots.default();
+                this.$helper.forEachForest<VNode>(allSlots, (slot: VNode) => {
+                    const slotType = slot.type;
+                    if (typeof slotType === "object" && (slotType as Record<string, unknown>).name) {
+                        formItems.push(slot);
+                    }
+                })
+            }
+            const formDom: HTMLElement = this.$el;
+            const labelDom = formDom.querySelector(".el-form-item__label") || document.body;
+            const styleList = window.getComputedStyle(labelDom);
+            const { font } = styleList;
+            const maxLabelWidth = Math.max.apply(Math, formItems.map((val) => {
+                const { props } = val;
+                const label: string = props ? (props.label || "") : "";
+                const labelWidth = this.$helper.getTextWidth(label, font)
+                return labelWidth;
+            }));
+            const realWidth = maxLabelWidth < 100 ? 100 : maxLabelWidth;
+            this.labelWidth = `${Math.ceil(realWidth)}px`
+        },
+        //初始化表单参数
+        initFormData() {
+            if (this.$slots.default) {
+                const allSlots = this.$slots.default();
+                this.$helper.forEachForest<VNode>(allSlots, (slot: VNode) => {
+                    const slotType = slot.type;
+                    const { props } = slot;
+                    if (typeof slotType === "object" && (slotType as Record<string, unknown>).name) {
+                        if (props && props.prop) {
+                            this.formInfo[props.prop] = null;
+                        }
+                    }
+                })
+            }
+        },
+        //初始化规则
+        initRules() {
+            if (this.$slots.default) {
+                const allSlots = this.$slots.default();
+                let rulePath: string;
+                this.$helper.forEachForest<VNode>(allSlots, (slot: VNode) => {
+                    const slotType = slot.type;
+                    const propsData = slot.props;
+                    if (typeof slotType === "object" && (slotType as Record<string, unknown>).name === "FormItem") {
+                        if (propsData && propsData.prop) {
+                            this.rules[propsData.prop] = [];
+                            rulePath = propsData.prop;
+                        }
+                        //填充必填项目rule规则
+                        if (propsData && (propsData.required === "" || propsData.required)) {
+                            const inputType = propsData.type;
+                            if (inputType == null || inputType === "input" || inputType === "textarea" || inputType === "remarks" || inputType === "password" || inputType === "number") { //普通输入框blur触发验证
+                                this.rules[rulePath].push({ required: true, message: `请输入${propsData.label}`, trigger: "blur" });
+                            } else if (inputType === "select" || inputType === "date" || inputType === "daterange") { //日期，单选，多选触发 blur
+                                this.rules[rulePath].push({ required: true, message: `请选择${propsData.label}`, trigger: "change" });
+                                this.rules[rulePath].push({ required: true, message: `请选择${propsData.label}`, trigger: "blur" });
+                            } else if (inputType === "upload") {
+                                this.rules[rulePath].push({ required: true, message: `请选择${propsData.label}`, trigger: "change" });
+                            } else if (inputType === "boolean") {
+                                this.rules[rulePath].push({ required: true, message: `请选择${propsData.label}`, trigger: ["blur", "change"] });
+                            } else if (inputType === "radio") {
+                                this.rules[rulePath].push({ required: true, message: `请选择${propsData.label}`, trigger: ["blur", "change"] });
+                            }
+                        }
+                        //填充字符最小长度规则
+                        if (propsData && propsData.minLen) {
+                            const inputType = propsData.type;
+                            if (inputType == null || inputType === "input" || inputType === "password") { //普通输入框blur触发验证
+                                this.rules[rulePath].push({ min: propsData.minLen, message: `${propsData.label}至少${propsData.minLen}位`, trigger: "blur" });
+                            }
+                        }
+                        //填充字符最大度规则
+                        if (propsData && propsData.maxLen) {
+                            const inputType = propsData.type;
+                            if (inputType == null || inputType === "input" || inputType === "password") { //普通输入框blur触发验证
+                                this.rules[rulePath].push({ max: propsData.maxLen, message: `${propsData.label}最多${propsData.maxLen}位`, trigger: "blur" });
+                            }
+                        }
+                        //填充字符长度规则
+                        if (propsData && propsData.len) {
+                            const inputType = propsData.type;
+                            if (inputType == null || inputType === "input" || inputType === "password") { //普通输入框blur触发验证
+                                this.rules[rulePath].push({ len: propsData.len, message: `${propsData.label}应该为${propsData.len}位`, trigger: "blur" });
+                            }
+                        }
+                        //填充email规则
+                        const validateEmail = (rule: unknown, value: string, callback: (err?: Error) => void) => {
+                            if (value === null || value.match(/\w+@.*\.\w+/)) {
+                                callback();
+                            } else {
+                                callback(new Error("请填写正确邮箱"));
+                            }
+                        };
+                        if (propsData && (propsData.email || propsData.email === "")) {
+                            const inputType = propsData.type;
+                            if (inputType == null || inputType === "input") { //普通输入框才能填充email规则
+                                this.rules[rulePath].push({ validator: validateEmail, trigger: "blur" });
+                            }
+                        }
+                        //填充手机号规则
+                        const validatePhone = (rule: unknown, value: string, callback: (err?: Error) => void) => {
+                            if (value === null || value.match(/^\d{11}$/)) {
+                                callback();
+                            } else {
+                                callback(new Error("请填写正确手机号"));
+                            }
+                        };
+                        if (propsData && (propsData.phone || propsData.phone === "")) {
+                            const inputType = propsData.type;
+                            if (inputType == null || inputType === "input") { //普通输入框才能填充手机号规则
+                                this.rules[rulePath].push({ validator: validatePhone, trigger: "blur" });
+                            }
+                        }
+                    }
+                })
+            }
+        },
+        //手动校验
+        validate(fn: () => void) {
+            (this.$refs.form as ElForm).validate(fn);
+        },
+        //重置表单
+        reset() {
+            (this.$refs.form as ElForm).resetFields();
+        },
         handleSubmit() {
             console.log(22)
         },
