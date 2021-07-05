d05d8efbd8f9245440d7fc640aaa5da909188366
shuxiaokai <2581105856@qq.com>
2021-07-05 21:50:18 +0800

    feat: 新增客户端路由页面

--- src/renderer/pages/modules/permission/client-routes/client-routes.vue
+++ src/renderer/pages/modules/permission/client-routes/client-routes.vue
@@ -6,21 +6,84 @@
 */
 <template>
     <div>
-        <s-fieldset title="前端路由">
-            aaa
-        </s-fieldset>
+        <!-- 搜索条件 -->
+        <s-search @change="handleChange">
+            <s-search-item label="名称&地址" prop="name"></s-search-item>
+            <template #operation>
+                <el-button type="success" size="mini">新增路由</el-button>
+                <el-button type="success" size="mini">批量修改类型</el-button>
+            </template>
+        </s-search>
+        <!-- 表格展示 -->
+        <s-table ref="table" url="/api/security/client_routes" :res-hook="hookRequest" :paging="false">
+            <el-table-column prop="name" label="路由名称" align="center"></el-table-column>
+            <el-table-column prop="path" label="路由地址" align="center"></el-table-column>
+            <el-table-column prop="groupName" label="分类名称" align="center"></el-table-column>
+            <el-table-column label="操作" align="center">
+                <template #default="scope">
+                    <el-button type="text" @click.stop="handleOpenClientEditDialog(scope.row)">修改</el-button>
+                    <el-button type="text" @click.stop="handleDeleteClientRoute(scope.row)">删除</el-button>
+                </template>
+            </el-table-column>
+        </s-table>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
+import { Response, ClientRoute } from "@@/global"
+type HookThis = {
+    tableInfo: unknown[],
+    total: number,
+}
 
 export default defineComponent({
     data() {
         return {
+            dialogVisible: false, //修改路由信息弹窗
+            loading: false, //
         };
     },
+    created() {
+        // this.getClientRoutes();
+    },
     methods: {
+        handleChange(params: Record<string, unknown>) {
+            this.$refs.table.getData(params);
+        },
+        //获取前端路由信息
+        hookRequest(res: Response<ClientRoute[]>, _this: HookThis) {
+            this.loading = true;
+            _this.tableInfo = res.data;
+            _this.total = res.data.length;
+            const uniqueData = this.$helper.uniqueByKey(res.data, "groupName");
+            console.log(uniqueData)
+        },
+        //打开修改前端路由组件
+        handleOpenClientEditDialog(row: ClientRoute) {
+            this.dialogVisible = true;
+            console.log(22, row)
+        },
+        //删除前端路由组件
+        handleDeleteClientRoute(row: ClientRoute) {
+            this.$confirm("此操作将永久删除此条记录, 是否继续?", "提示", {
+                confirmButtonText: "确定",
+                cancelButtonText: "取消",
+                type: "warning",
+            }).then(() => {
+                const params = { ids: [row._id] };
+                this.axios.delete("/api/security/client_routes", { data: params }).then(() => {
+                    this.$refs.table.getData();
+                }).catch((err) => {
+                    console.error(err);
+                });
+            }).catch((err: Error | string) => {
+                if (err === "cancel" || err === "close") {
+                    return;
+                }
+                console.error(err);
+            });
+        },
     },
 })
 </script>
