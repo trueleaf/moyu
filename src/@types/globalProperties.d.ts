77fae0d90c940801e1eb9fa86457129551ac4c10
shuxiaokai <2581105856@qq.com>
2021-07-12 18:02:06 +0800

    feat: 解决.vue文件引入报错

---
+++ src/@types/globalProperties.d.ts
@@ -0,0 +1,48 @@
+
+import { Store } from "vuex"
+import { AxiosInstance } from "axios"
+import { State } from "@@/store"
+import { Helper } from "@@/helper"
+import { ElMessageBoxShortcutMethod, ElMessage } from "element-plus"
+import { TreeNodeOptions } from "element-plus/packages/tree/src/tree.type"
+import { IElDropdownInstance } from "element-plus/packages/dropdown/src/dropdown"
+
+type Data = Record<string, unknown>;
+
+declare module "@vue/runtime-core" {
+    export interface ComponentCustomProperties {
+        $refs: {
+            form: {
+                validate: (fn: (valid: boolean) => void) => void,
+                validateField: (field: string) => void,
+                resetFields: () => void,
+                $el: HTMLElement,
+                formInfo: Data,
+            }
+            table: {
+                /**
+                 * 获取表格数据
+                 */
+                getData: (params?: Data) => void,
+                /**
+                 * 表格数据
+                 */
+                tableData: unknown[],
+                /**
+                 * 表格数据总量
+                 */
+                total: number,
+                $el: HTMLElement,
+            },
+            tree: TreeNodeOptions["store"],
+            dropdown: IElDropdownInstance,
+        },
+        $store: Store<State>,
+        $confirm: typeof ElMessageBoxShortcutMethod,
+        $message: typeof ElMessage,
+        $nextTick: (fn: () => void) => void,
+        axios: AxiosInstance,
+        $helper: Helper,
+        $set(target: Data, key: string, value: unknown): void,
+    }
+}
