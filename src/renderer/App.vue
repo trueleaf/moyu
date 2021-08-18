7b1b718ed90ef848e732d4e1c8a91865acfec822
shuxiaokai <2581105856@qq.com>
2021-08-18 18:10:32 +0800

    upd: 更新国际化引入方式

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,15 +5,23 @@
     备注：
 */
 <template>
-    <router-view></router-view>
+    <el-config-provider :locale="locale">
+        <router-view></router-view>
+    </el-config-provider>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
+import { ElConfigProvider } from "element-plus"
+import zhCn from "element-plus/lib/locale/lang/zh-cn"
 
 export default defineComponent({
+    components: {
+        [ElConfigProvider.name]: ElConfigProvider,
+    },
     data() {
         return {
+            locale: zhCn,
         };
     },
     methods: {
--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue"
 import ElementPlus from "element-plus";
-import zhLocale from "element-plus/lib/locale/lang/zh-cn"
 import mixin from "@/mixin/index"
 import { cache } from "./cache/cache"
 import App from "./App.vue"
@@ -12,6 +11,8 @@ import { router } from "./router"
 import { store, key } from "./store"
 import { registeGlobalComponent } from "@/components"
 import "@/assets/css/index.css"
+import registeDirective from "./directive/directive";
+
 const app = createApp(App, {
     mixin: [mixin]
 })
@@ -20,6 +21,7 @@ app.config.globalProperties.$helper = helper; //挂载全局辅助函数
 app.config.globalProperties.$cache = cache; //挂载全局storage方法
 
 registeGlobalComponent(app); //注册全局组件
+registeDirective(app); //注册全局指令
 app.use(store, key);
-app.use(axiosPlugin).use(ElementPlus, { locale: zhLocale }).use(router);
+app.use(axiosPlugin).use(ElementPlus).use(router);
 app.mount("#app")
