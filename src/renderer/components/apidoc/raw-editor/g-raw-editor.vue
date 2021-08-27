2dd5b496b0eaacb8b4b3627f1bddb92f3b562806
shuxiaokai <2581105856@qq.com>
2021-08-27 19:41:07 +0800

    feat: 新增ace编辑器

---
+++ src/renderer/components/apidoc/raw-editor/g-raw-editor.vue
@@ -0,0 +1,90 @@
+/*
+    创建者：shuxiaokai
+    创建时间：2021-08-27 21:17
+    模块名称：raw类型数据编辑器
+    备注：
+*/
+<template>
+    <pre id="editor"></pre>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue"
+import ace, { Editor} from "brace";
+import "brace/mode/json";
+import "brace/mode/javascript";
+import "brace/mode/html";
+import "brace/mode/xml";
+import "brace/mode/text";
+import "brace/theme/github";
+import type { ApidocBodyRawType } from "@@/global"
+
+const TYPE_MAP = {
+    "text/plain": "text",
+    "text/html": "html",
+    "application/xml": "xml",
+    "application/json": "json",
+    "text/javascript": "javascript"
+}
+
+export default defineComponent({
+    props: {
+        type: {
+            type: String as PropType<ApidocBodyRawType>,
+            default: "javascript",
+        },
+        value: {
+            type: String,
+            default: "",
+        },
+    },
+    emits: ["input", "ready"],
+    data() {
+        return {
+            editorInstance: null as null | Editor,
+        };
+    },
+    watch: {
+        type: {
+            handler(type: ApidocBodyRawType) {
+                if (this.editorInstance) {
+                    console.log(TYPE_MAP[type], type)
+                    this.editorInstance.getSession().setMode(`ace/mode/${TYPE_MAP[type]}`);
+                }
+            },
+            immediate: true,
+        },
+    },
+    mounted() {
+        this.initEditor();
+    },
+    methods: {
+        initEditor() {
+            this.editorInstance = ace.edit(this.$el);
+            this.editorInstance.getSession().setMode(`ace/mode/${TYPE_MAP[this.type] || "text"}`);
+            this.editorInstance.setTheme("ace/theme/github");
+            this.editorInstance.setOptions({
+                // enableBasicAutocompletion: true,
+                // enableSnippets: true,
+                // enableLiveAutocompletion: false
+            });
+            this.editorInstance.on("change", () => {
+                const content = this.editorInstance?.getValue();
+                this.$emit("input", content);
+            });
+            this.$emit("ready", this.editorInstance);
+        },
+        setValue(value: string) {
+            this.editorInstance?.setValue(value);
+            this.editorInstance?.clearSelection();
+        },
+    },
+})
+</script>
+
+<style lang="scss">
+#editor {
+    width: 100%;
+    height: 90%;
+}
+</style>
