5ec80506065dce0c37d36af8800f5a8329148f06
shuxiaokai <2581105856@qq.com>
2021-09-14 18:36:06 +0800

    fix: 修复发送请求返回值无加载效果

--- src/renderer/pages/modules/apidoc/doc-edit/content/apidoc/response/response.vue
+++ src/renderer/pages/modules/apidoc/doc-edit/content/apidoc/response/response.vue
@@ -7,33 +7,35 @@
 <template>
     <s-base-info v-show="layout === 'horizontal'"></s-base-info>
     <s-res-info v-show="layout === 'horizontal'"></s-res-info>
-    <div v-show="remoteResponse.data.type" class="remote-response-wrap px-3" :class="{ vertical: layout === 'vertical' }">
-        <el-tabs v-model="activeName" class="h-100">
-            <el-tab-pane label="返回值" name="s-body">
-                <s-body class="h-100"></s-body>
-            </el-tab-pane>
-            <el-tab-pane name="s-cookie">
-                <template #label>
-                    <span>Cookie&nbsp;</span>
-                    <span v-if="cookies.length > 0" class="orange">({{ cookies.length }})</span>
-                </template>
-                <!-- fix: 文字隐藏组件获取dom宽度失败 -->
-                <s-cookie v-if="activeName === 's-cookie'"></s-cookie>
-            </el-tab-pane>
-            <el-tab-pane name="s-headers">
-                <template #label>
-                    <span>返回头&nbsp;</span>
-                    <span v-if="headers.length > 0" class="orange">({{ headers.length }})</span>
-                </template>
-                <s-headers></s-headers>
-            </el-tab-pane>
-        </el-tabs>
-    </div>
-    <el-empty v-show="!remoteResponse.data.type">
-        <template #description>
-            <span>点击发送按钮发送请求</span>
-        </template>
-    </el-empty>
+    <s-loading :loading="requestLoading">
+        <div v-show="remoteResponse.data.type" class="remote-response-wrap px-3" :class="{ vertical: layout === 'vertical' }">
+            <el-tabs v-model="activeName" class="h-100">
+                <el-tab-pane label="返回值" name="s-body">
+                    <s-body class="h-100"></s-body>
+                </el-tab-pane>
+                <el-tab-pane name="s-cookie">
+                    <template #label>
+                        <span>Cookie&nbsp;</span>
+                        <span v-if="cookies.length > 0" class="orange">({{ cookies.length }})</span>
+                    </template>
+                    <!-- fix: 文字隐藏组件获取dom宽度失败 -->
+                    <s-cookie v-if="activeName === 's-cookie'"></s-cookie>
+                </el-tab-pane>
+                <el-tab-pane name="s-headers">
+                    <template #label>
+                        <span>返回头&nbsp;</span>
+                        <span v-if="headers.length > 0" class="orange">({{ headers.length }})</span>
+                    </template>
+                    <s-headers></s-headers>
+                </el-tab-pane>
+            </el-tabs>
+        </div>
+        <el-empty v-show="!remoteResponse.data.type">
+            <template #description>
+                <span>点击发送按钮发送请求</span>
+            </template>
+        </el-empty>
+    </s-loading>
 </template>
 
 <script lang="ts" setup>
@@ -67,7 +69,9 @@ const layout = computed(() => {
 const remoteResponse = computed(() => {
     return store.state["apidoc/response"]
 })
-
+const requestLoading = computed(() => {
+    return store.state["apidoc/response"].loading
+})
 </script>
 
 <style lang="scss">
