a2cc55f56c8e842a498cf318d9379f18fd253728
shuxiaokai <2581105856@qq.com>
2021-09-23 18:01:04 +0800

    upd: 更新引导页面文案

--- src/renderer/pages/modules/apidoc/doc-edit/content/guide/guide.vue
+++ src/renderer/pages/modules/apidoc/doc-edit/content/guide/guide.vue
@@ -5,7 +5,17 @@
     备注：
 */
 <template>
-    <div>guide</div>
+    <div class="guide">
+        <div class="wrap">
+            <img :src="require('@/assets/imgs/logo.png')" width="150" height="150" alt="logo图片" class="logo">
+            <h2>当前版本0.8.0</h2>
+            <p>预计将会在年底发布第一个稳定版本！</p>
+            <!-- <ul>
+                <li>支持web端调试</li>
+                <li>支持表单</li>
+            </ul> -->
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -22,5 +32,15 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-
+.guide {
+    height: calc(100vh - #{size(100)});
+    position: relative;
+    .wrap {
+        text-align: center;
+        position: absolute;
+        top: 10%;
+        left: 50%;
+        transform: translate(-50%, 0);
+    }
+}
 </style>
