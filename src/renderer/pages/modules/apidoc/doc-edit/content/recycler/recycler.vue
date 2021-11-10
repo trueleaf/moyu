4fc58a44d677161c473164d85b7b29a5437f4a7e
shuxiaokai <2581105856@qq.com>
2021-11-10 17:47:16 +0800

    upd: 更新回收站功能

--- src/renderer/pages/modules/apidoc/doc-edit/content/recycler/recycler.vue
+++ src/renderer/pages/modules/apidoc/doc-edit/content/recycler/recycler.vue
@@ -6,13 +6,13 @@
 */
 <template>
     <div class="recycler">
-        {{ formInfo }}
+        <!-- 过滤条件 -->
         <s-fieldset title="过滤条件" class="search">
             <!-- 操作人员 -->
             <div class="op-item">
                 <div>操作人员：</div>
                 <el-checkbox-group v-model="formInfo.operators">
-                    <el-checkbox v-for="(item, index) in memberEnum" :key="index" :label="item"></el-checkbox>
+                    <el-checkbox v-for="(item, index) in memberEnum" :key="index" :label="item.name"></el-checkbox>
                     <el-button type="text" @click="handleClearOperator">清空</el-button>
                 </el-checkbox-group>
             </div>
@@ -59,14 +59,65 @@
                 </div>
             </div>
         </s-fieldset>
+        <!-- 列表展示 -->
+        <s-loading v-if="deletedList.length > 0" :loading="loading" class="list">
+            <div v-for="(item, index) in deletedInfo" :key="index" class="list-wrap">
+                <h2 class="title">{{ item.title }}</h2>
+                <div class="oneday-wrap">
+                    <div v-for="(chunkDeleteInfo, key) in item.deleted" :key="key" class="date-chunk">
+                        <h3 class="date my-2">{{ $helper.formatDate(key, "a HH:mm") }}</h3>
+                        <div class="date-list-wrap">
+                            <div v-for="(docInfo, index3) in chunkDeleteInfo" :key="index3" class="docinfo">
+                                <div class="op-area mr-4">
+                                    <el-button type="text" :loading="loading2" @click="handleRestore(docInfo)">恢复</el-button>
+                                    <el-divider direction="vertical"></el-divider>
+                                    <el-popover v-model:visible="docInfo._visible" placement="right" width="auto" trigger="manual" transition="none">
+                                        <doc-detail v-if="docInfo._visible" :id="docInfo._id" @close="docInfo._visible = false;"></doc-detail>
+                                        <template #reference>
+                                            <el-button type="text" @click.stop="handleShowDetail(docInfo)">详情</el-button>
+                                        </template>
+                                    </el-popover>
+                                </div>
+                                <div class="operator mr-1">{{ docInfo.deletePerson }}</div>
+                                <div class="mr-2">删除了</div>
+                                <div v-if="docInfo.isFolder" class="d-flex a-center">
+                                    <img :src="require('@/assets/imgs/apidoc/folder.png')" width="16" height="16" class="mr-1" />
+                                    <span>{{ docInfo.name }}</span>
+                                </div>
+                                <div v-else class="d-flex a-center">
+                                    <img :src="require('@/assets/imgs/apidoc/file.png')" width="16" height="16" class="mr-1" />
+                                    <span class="mr-2">{{ docInfo.name }}</span>
+                                    <template v-for="(req) in validRequestMethods">
+                                        <span v-if="docInfo.method === req.value.toLowerCase()" :key="req.value" class="mr-1" :style="{color: req.iconColor}">{{ req.name }}</span>
+                                    </template>
+                                    <span>{{ docInfo.path }}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </s-loading>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, Ref, onMounted, watch } from "vue"
-import type { ApidocHttpRequestMethod, ApidocType, ResponseTable } from "@@/global"
+import { ref, Ref, onMounted, watch, computed } from "vue"
+import dayjs from "dayjs"
+import isToday from "dayjs/plugin/isToday"
+import isYesterday from "dayjs/plugin/isYesterday"
+import "dayjs/locale/zh-cn"
+import { ElMessageBox } from "element-plus"
+import type { ApidocHttpRequestMethod, ApidocType, ResponseTable, ApidocProjectPermission } from "@@/global"
 import { router } from "@/router/index"
 import { axios } from "@/api/api"
+import { store } from "@/store/index"
+import { forEachForest, debounce } from "@/helper"
+import docDetail from "./components/doc-detail.vue"
+
+dayjs.extend(isYesterday)
+dayjs.extend(isToday)
+dayjs.locale("zh-cn")
 
 type DeleteInfo = {
     _id: string, //项目id
@@ -79,6 +130,7 @@ type DeleteInfo = {
     pid: string, //父元素id
     type: ApidocType, //文档类型
     updatedAt: string, //更新时间
+    _visible?: boolean,
 };
 type SearchInfo = {
     projectId: string, //项目id
@@ -89,8 +141,9 @@ type SearchInfo = {
     operators: string[], //----操作者信息
 }
 
+const projectId = router.currentRoute.value.query.id as string; //项目id
 const formInfo: Ref<SearchInfo> = ref({
-    projectId: router.currentRoute.value.query.id as string, //项目id
+    projectId, //项目id
     startTime: null, //--起始日期
     endTime: null, //----结束日期
     docName: "", //---------请求名称
@@ -122,16 +175,16 @@ const getData = () => {
 | 搜索相关内容
 |--------------------------------------------------------------------------
 */
-const memberEnum: Ref<string[]> = ref([]); //操作人员
+const memberEnum: Ref<{ name: string, permission:ApidocProjectPermission }[]> = ref([]); //操作人员
 const dateRange: Ref<string> = ref(""); //日期范围
 const customDateRange: Ref<number[]> = ref([]); //自定义日期范围
 //获取操作人员枚举
 const getOperatorEnum = () => {
     const params = {
-        projectId: router.currentRoute.value.query.id as string,
+        projectId,
     };
     axios.get("/api/docs/docs_history_operator_enum", { params }).then((res) => {
-        memberEnum.value = res.data as string[];
+        memberEnum.value = res.data as { name: string, permission:ApidocProjectPermission }[];
     }).catch((err) => {
         console.error(err);
     });
@@ -193,11 +246,150 @@ watch(() => customDateRange.value, (val) => {
         formInfo.value.endTime = val[1];
     }
 })
+
+const debounceFn: Ref<(() => void) | null> = ref(null);
+watch(() => formInfo.value, () => {
+    if (!debounceFn.value) {
+        debounceFn.value = debounce(() => {
+            getData();
+        });
+    }
+    if (debounceFn.value) {
+        debounceFn.value();
+    }
+}, {
+    deep: true
+})
 onMounted(() => {
     getData();
     getOperatorEnum();
 })
+/*
+|--------------------------------------------------------------------------
+| 列表数据
+|--------------------------------------------------------------------------
+*/
+//被删除数据
+const deletedInfo = computed(() => {
+    const result: Record<string, {
+        title: string,
+        deleted: Record<string, DeleteInfo[]>
+    }> = {};
+    deletedList.value.forEach((item) => {
+        const { updatedAt } = item;
+        const ymdString = dayjs(updatedAt).format("YYYY-MM-DD");
+        const ymdhmString = dayjs(updatedAt).format("YYYY-MM-DD HH:mm");
+        if (!result[ymdString]) {
+            let title = "";
+            if (dayjs(updatedAt).isToday()) {
+                title = "今天"
+            } else if (dayjs(updatedAt).isYesterday()) {
+                title = "昨天"
+            } else {
+                title = dayjs(updatedAt).format("YYYY年M月DD号");
+            }
+            result[ymdString] = {
+                title,
+                deleted: {},
+            };
+        }
+        if (!result[ymdString].deleted[ymdhmString]) {
+            result[ymdString].deleted[ymdhmString] = [];
+        }
+        result[ymdString].deleted[ymdhmString].push(item);
+    })
+    return result;
+})
+//请求方法
+const validRequestMethods = computed(() => store.state["apidoc/baseInfo"].rules.requestMethods)
 
+/*
+|--------------------------------------------------------------------------
+| 列表数据相关操作
+|--------------------------------------------------------------------------
+*/
+const loading2 = ref(false); //回复按钮
+//恢复接口
+const restoreDocDirectly = (docInfo: DeleteInfo) => {
+    ElMessageBox.confirm(`确实要恢复 ${docInfo.name} 吗?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+    }).then(() => {
+        loading2.value = true;
+        const params = {
+            _id: docInfo._id,
+            projectId,
+        };
+        axios.put("/api/docs/docs_restore", params).then((res) => {
+            const delIds = res.data;
+            for (let i = 0; i < delIds.length; i += 1) {
+                const id = delIds[i];
+                console.log(id)
+                const delIndex = deletedList.value.findIndex((val) => val._id === id);
+                deletedList.value.splice(delIndex, 1)
+            }
+            store.dispatch("apidoc/banner/getDocBanner", {
+                projectId,
+            })
+        }).catch((err) => {
+            console.error(err);
+        }).finally(() => {
+            loading2.value = false;
+        });
+    }).catch((err) => {
+        if (err === "cancel" || err === "close") {
+            return;
+        }
+        console.error(err);
+    });
+}
+//恢复接口
+const handleRestore = (docInfo: DeleteInfo) => {
+    const { banner } = store.state["apidoc/banner"];
+    const { pid, isFolder } = docInfo;
+    let hasParent = false;
+    forEachForest(banner, (node) => {
+        if (node._id === pid) {
+            hasParent = true;
+        }
+    });
+    if (!pid && !isFolder) { //文档，根元素
+        restoreDocDirectly(docInfo)
+    } else if (pid && !isFolder && hasParent) { //文档，非根元素,存在父元素
+        restoreDocDirectly(docInfo)
+    } else if (pid && !isFolder && !hasParent) { //文档，非根元素,不存在父元素
+        restoreDocDirectly(docInfo)
+    } else {
+        restoreDocDirectly(docInfo)
+    }
+}
+//查看详情
+const handleShowDetail = (docInfo: DeleteInfo) => {
+    Object.keys(deletedInfo.value).forEach((key) => {
+        const el = deletedInfo.value[key];
+        Object.keys(el.deleted).forEach((key2) => {
+            const el2 = el.deleted[key2];
+            el2.forEach((info) => {
+                info._visible = false;
+            })
+        })
+    })
+    docInfo._visible = true;
+};
+onMounted(() => {
+    document.documentElement.addEventListener("click", () => {
+        Object.keys(deletedInfo.value).forEach((key) => {
+            const el = deletedInfo.value[key];
+            Object.keys(el.deleted).forEach((key2) => {
+                const el2 = el.deleted[key2];
+                el2.forEach((info) => {
+                    info._visible = false;
+                })
+            })
+        })
+    })
+})
 </script>
 
 <style lang="scss">
