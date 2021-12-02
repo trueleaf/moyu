eb59f9e4f406572589529ec1d51c777616ec2622
shuxiaokai <2581105856@qq.com>
2021-12-02 17:58:41 +0800

    feat: 新增简单备注信息

---
+++ src/renderer/pages/modules/apidoc/doc-edit/content/apidoc/params/remarks/remarks.vue
@@ -0,0 +1,27 @@
+/*
+    创建者：shuxiaokai
+    创建时间：2021-12-02 21:22
+    模块名称：
+    备注：
+*/
+<template>
+    <el-input v-model="description" rows="4" type="textarea" show-word-limit name="name" size="mini" placeholder="在此处输入备注信息" class="w-100" maxlength="255" clearable></el-input>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue"
+import { store } from "@/store/index"
+
+const description = computed({
+    get() {
+        return store.state["apidoc/apidoc"].apidoc.info.description
+    },
+    set(val: string) {
+        store.commit("apidoc/apidoc/changeDescription", val)
+    }
+})
+</script>
+
+<style lang="scss">
+
+</style>
