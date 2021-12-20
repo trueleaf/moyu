82280dfc654450c9d3601443e40fc6b6aad9b895
shuxiaokai <2581105856@qq.com>
2021-12-20 18:15:14 +0800

    upd: 更新编辑器提示文案

--- src/renderer/components/apidoc/monaco-editor/registerCompletionItem.ts
+++ src/renderer/components/apidoc/monaco-editor/registerCompletionItem.ts
@@ -1,20 +1,13 @@
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 
-const suggestions = [{
-    label: {
-        label: "pm",
-        description: "全局对象"
-    },
-    kind: monaco.languages.CompletionItemKind.Function,
-    insertText: "pm",
-    trigger: ["p"],
-}, {
+const variableSuggestion = [{
     label: {
         label: "variables",
-        description: "临时变量对象"
+        description: "临时变量"
     },
-    kind: monaco.languages.CompletionItemKind.Property,
+    kind: monaco.languages.CompletionItemKind.Function,
     insertText: "variables",
+    sortText: "1",
     trigger: ["pm."]
 }, {
     label: {
@@ -54,19 +47,11 @@ const suggestions = [{
     trigger: ["pm.variables."]
 }, {
     label: {
-        label: "getAll",
-        description: "获取所有临时变量"
-    },
-    kind: monaco.languages.CompletionItemKind.Function,
-    insertText: "getAll",
-    trigger: ["pm.variables."]
-}, {
-    label: {
         label: "has",
         description: "判断临时变量是否存在"
     },
     kind: monaco.languages.CompletionItemKind.Function,
-    insertText: "has(变量名称)",
+    insertText: `has("变量名称")`,
     trigger: ["pm.variables."]
 }, {
     label: {
@@ -92,13 +77,102 @@ const suggestions = [{
     kind: monaco.languages.CompletionItemKind.Function,
     insertText: "toObject",
     trigger: ["pm.variables."]
+}]
+const collectionVariableSuggestion = [{
+    label: {
+        label: "collectionVariables",
+        description: "集合内变量(跨接口使用)"
+    },
+    kind: monaco.languages.CompletionItemKind.Function,
+    insertText: "collectionVariables",
+    sortText: "2",
+    trigger: ["pm."]
 }, {
+    label: {
+        label: "get",
+        description: "获取单个集合内变量"
+    },
+    kind: monaco.languages.CompletionItemKind.Function,
+    insertText: `get("变量名称")`,
+    sortText: "1",
+    trigger: ["pm.collectionVariables."]
+}, {
+    label: {
+        label: "set",
+        description: "设置集合内变量值"
+    },
+    kind: monaco.languages.CompletionItemKind.Function,
+    insertText: `set("变量名称", "变量值")`,
+    sortText: "2",
+    trigger: ["pm.collectionVariables."]
+}, {
+    label: {
+        label: "update",
+        description: "更新集合内变量"
+    },
+    kind: monaco.languages.CompletionItemKind.Function,
+    insertText: `update("变量名称", "变量值")`,
+    sortText: "3",
+    trigger: ["pm.collectionVariables."]
+}, {
+    label: {
+        label: "upsert",
+        description: "更新集合内变量(不存在则新增)"
+    },
+    kind: monaco.languages.CompletionItemKind.Function,
+    insertText: `upsert("变量名称", "变量值")`,
+    sortText: "4",
+    trigger: ["pm.collectionVariables."]
+}, {
+    label: {
+        label: "has",
+        description: "判断集合内变量是否存在"
+    },
+    kind: monaco.languages.CompletionItemKind.Function,
+    insertText: `has("变量名称")`,
+    trigger: ["pm.collectionVariables."]
+}, {
+    label: {
+        label: "unset",
+        description: "删除集合内变量值(同delete)"
+    },
+    kind: monaco.languages.CompletionItemKind.Function,
+    insertText: `unset("变量名称")`,
+    trigger: ["pm.collectionVariables."]
+}, {
+    label: {
+        label: "delete",
+        description: "删除集合内变量值(同unset)"
+    },
+    kind: monaco.languages.CompletionItemKind.Function,
+    insertText: `delete("变量名称")`,
+    trigger: ["pm.collectionVariables."]
+}, {
+    label: {
+        label: "toObject",
+        description: "以对象形式输出集合内变量"
+    },
+    kind: monaco.languages.CompletionItemKind.Function,
+    insertText: "toObject",
+    trigger: ["pm.collectionVariables."]
+}]
+
+const suggestions = [{
+    label: {
+        label: "pm",
+        description: "全局对象"
+    },
+    kind: monaco.languages.CompletionItemKind.Function,
+    insertText: "pm",
+    trigger: ["p"],
+}, ...variableSuggestion, ...collectionVariableSuggestion, {
     label: {
         label: "request",
         description: "全局请求"
     },
     kind: monaco.languages.CompletionItemKind.Method,
     insertText: "request",
+    sortText: "3",
     trigger: ["pm."]
 }, {
     label: {
