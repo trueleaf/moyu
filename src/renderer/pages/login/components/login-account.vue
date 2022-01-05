0fbf81e19e78ac1352cbf0cbdae67f62d36866e6
shuxiaokai <2581105856@qq.com>
2022-01-05 17:58:35 +0800

    remove: 移除无用代码

--- src/renderer/pages/login/components/login-account.vue
+++ src/renderer/pages/login/components/login-account.vue
@@ -111,7 +111,7 @@ export default defineComponent({
             this.$refs.form.validate((valid: boolean) => {
                 if (valid) {
                     this.loading = true;
-                    this.axios.post<Response<PermissionUserInfo>, Response<PermissionUserInfo>>("/api/security/login_password", this.userInfo).then((res: Response<PermissionUserInfo>) => {
+                    this.axios.post<Response<PermissionUserInfo>, Response<PermissionUserInfo>>("/api/security/login_password", this.userInfo).then((res) => {
                         if (res.code === 2006 || res.code === 2003) {
                             this.$message.warning(res.msg);
                             this.isShowCapture = true;
