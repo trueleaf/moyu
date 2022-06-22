76ea8b077c9c70e71bde5c7104b5cc94c1012e13
shuxiaokai <2581105856@qq.com>
2022-06-22 22:50:30 +0800

    upd: 新增对json数据增删改查支持

---
+++ public/sandbox/pre-request/request/json.js
@@ -0,0 +1,33 @@
+/**
+ * 路径参数
+ */
+const _json = {}; //为了初始化的时候不执行赋值操作
+const jsonValidator = {
+    get(target, key) {
+        if (typeof target[key] === "object" && target[key] !== null) {
+            return new Proxy(target[key], jsonValidator)
+        } else {
+            return target[key];
+        }
+    },
+    set(obj, prop, value) {
+        if (typeof value === "object" && value !== null) {
+            obj[prop] = new Proxy(value, jsonValidator);
+        } else {
+            obj[prop] = value;
+        }
+        self.postMessage({
+            type: "prerequest-change-json-params",
+            value: JSON.stringify(json),
+        });
+        return true;
+    },
+    deleteProperty(target, prop) {
+        delete target[prop];
+        self.postMessage({
+            type: "prerequest-change-json-params",
+            value: JSON.stringify(json),
+        });
+    },
+}
+const json = new Proxy(_json, jsonValidator);
