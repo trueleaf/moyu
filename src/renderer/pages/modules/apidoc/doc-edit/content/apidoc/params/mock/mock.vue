e3ebc569360b6852d1605553edea3bfa2d599818
shuxiaokai <2581105856@qq.com>
2022-07-11 22:23:21 +0800

    upd: 更新mock服务器基本操作

--- src/renderer/pages/modules/apidoc/doc-edit/content/apidoc/params/mock/mock.vue
+++ src/renderer/pages/modules/apidoc/doc-edit/content/apidoc/params/mock/mock.vue
@@ -7,11 +7,33 @@
 <template>
     <div class="mock-wrap">
         <s-fieldset title="Mock服务器基本信息" class="w-80">
+            <s-label-value label="状态：" label-width="50px" class="mb-1" one-line>
+                <span v-if="mockInfo.serverState === 'closing'" class="f-sm">
+                    <span class="dot"></span>
+                    <span>关闭中</span>
+                </span>
+                <span v-if="mockInfo.serverState === 'connecting'" class="f-sm">
+                    <span class="dot bg-orange"></span>
+                    <span>连接中</span>
+                </span>
+                <span v-if="mockInfo.serverState === 'connection'" class="f-sm">
+                    <span class="dot bg-green"></span>
+                    <span>已连接</span>
+                </span>
+                <span v-if="mockInfo.serverState === 'disconnection'" class="f-sm">
+                    <span class="dot bg-gray-500"></span>
+                    <span>断开连接</span>
+                </span>
+                <span v-if="mockInfo.serverState === 'error'" class="f-sm">
+                    <span class="dot bg-red"></span>
+                    <span>异常</span>
+                </span>
+            </s-label-value>
             <s-label-value label="Mock地址：" label-width="90px" class="mb-1" one-line>
-                <span class="text">{{ originMockUrl }}</span>
+                <span class="text">{{ mockServer }}</span>
                 <input v-model="customPath" type="text" class="edit-ipt">
                 <span v-copy="fullMockUrl" class="cursor-pointer f-xs theme-color ml-1 mr-2">复制</span>
-                <span class="theme-color cursor-pointer f-xs">还原</span>
+                <span class="theme-color cursor-pointer f-xs" @click="handleResetMockUrl">还原</span>
             </s-label-value>
             <s-label-value label="Mock端口：" label-width="90px" class="mb-1" one-line>
                 <span v-if="!isEditing">{{ mockPort }}</span>
@@ -29,8 +51,8 @@
                 <el-tab-pane label="自定义返回结果" name="response"></el-tab-pane>
                 <el-tab-pane label="自定义返回头" name="header"> </el-tab-pane>
             </el-tabs>
-            <button v-if="0" @click="handleShutdownMockServer">关闭服务器</button>
-            <button v-if="0" @click="handleOpenMockServer">开启服务器</button>
+            <button @click="handleShutdownMockServer">关闭服务器</button>
+            <button @click="handleOpenMockServer">开启服务器</button>
             <pre>{{ mockInfo }}</pre>
             <!-- <pre>{{ fullMockUrl }}</pre> -->
         </s-fieldset>
@@ -47,7 +69,7 @@ import globalConfig from "@/../config/config"
 
 const mockInfo = computed(() => store.state["apidoc/mock"]); //mock服务器相关数据
 const mockPort = ref(mockInfo.value.mockServerPort);
-const _mockPort = mockPort.value;
+const _mockPort = ref(mockPort.value);
 const isEditing = ref(false);
 
 /*
@@ -55,11 +77,15 @@ const isEditing = ref(false);
 | 请求url
 |--------------------------------------------------------------------------
 */
-const originMockUrl = ref(`http://${globalConfig.renderConfig.mock.ip}:${store.state["apidoc/mock"].mockServerPort}`);
+const mockServer = computed(() => `http://${globalConfig.renderConfig.mock.ip}:${store.state["apidoc/mock"].mockServerPort}`);
 const orginPath = computed(() => store.state["apidoc/apidoc"].apidoc.item.url.path); //原始请求路径
 const customPath = ref(""); //自定义请求路径
-const fullMockUrl = computed(() => `${originMockUrl.value}${customPath.value}`); //完整请求url
+const fullMockUrl = computed(() => `${mockServer.value}${customPath.value}`); //完整请求url
 const apidocInfo = computed(() => store.state["apidoc/apidoc"].apidoc); //当前api文档信息
+//重置mock数据
+const handleResetMockUrl = () => {
+    customPath.value = orginPath.value;
+}
 watch(orginPath, (newVal) => {
     if (customPath.value === "") {
         customPath.value = newVal;
@@ -68,23 +94,13 @@ watch(orginPath, (newVal) => {
 watch(customPath, (newVal) => {
     const { urlMap } = store.state["apidoc/mock"];
     const matchedMockInfo = urlMap.find(v => v.id === apidocInfo.value._id);
-    const matchedCustomMockInfo = urlMap.find(v => v.id === apidocInfo.value._id && v.isCustom);
-    if (matchedMockInfo && !matchedCustomMockInfo) { //如不不存在默认数据，则新增一条数据
-        store.commit("apidoc/mock/addMockUrl", {
+    store.commit("apidoc/mock/changeMockUrlInfoById", {
+        id: matchedMockInfo?.id,
+        data: {
             ...matchedMockInfo,
             url: newVal,
-            isCustom: true,
-        })
-    } else if (matchedMockInfo && matchedCustomMockInfo) {
-        store.commit("apidoc/mock/changeMockUrlInfoById", {
-            id: matchedMockInfo.id,
-            data: {
-                ...matchedMockInfo,
-                url: newVal,
-            }
-        })
-    }
-    // console.log(newVal, matchedMockInfo)
+        }
+    })
 })
 /*
 |--------------------------------------------------------------------------
@@ -100,6 +116,10 @@ const handleChangePortEditState = () => {
 //改变端口
 const handleChangePort = () => {
     store.commit("apidoc/mock/changeMockServerPort", mockPort.value);
+    if (mockInfo.value.serverState === "connection") {
+        event.emit("apidoc/mock/restartMockServer")
+    }
+    _mockPort.value = mockPort.value;
     isEditing.value = false;
 }
 //关闭mock服务器
@@ -119,6 +139,13 @@ onBeforeUnmount(() => {
 <style lang="scss" scoped>
 .mock-wrap {
     font-size: fz(15);
+    .dot {
+        display: inline-block;
+        width: size(10);
+        height: size(10);
+        border-radius: 50%;
+        margin-right: size(5);
+    }
     .text {
         display: inline-block;
         height: size(30);
