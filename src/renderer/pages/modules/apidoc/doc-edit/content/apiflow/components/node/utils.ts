8a080de1cddb1e354cc3ad4e2e147b7095ea00db
shuxiaokai <2581105856@qq.com>
2022-09-26 21:18:56 +0800

    upd: 更新线段计算方式

--- src/renderer/pages/modules/apidoc/doc-edit/content/apiflow/components/node/utils.ts
+++ src/renderer/pages/modules/apidoc/doc-edit/content/apiflow/components/node/utils.ts
@@ -22,8 +22,10 @@ type ResultRect = {
 
 //根据起始位置返回节点 width height left top
 export function getRectInfo(startInfo: Coordinate, endInfo: Coordinate): ResultRect {
-    const arrowWidth = 15;
-    const arrowHeight = 5;
+    const arrowLength = 15;
+    const arrowWidth = 5;
+    const minWidth = 20
+    const minHeight = 20
     const result: ResultRect = {
         x: 0,
         y: 0,
@@ -50,50 +52,179 @@ export function getRectInfo(startInfo: Coordinate, endInfo: Coordinate): ResultR
             }
         },
     }
+    if (Math.abs(endInfo.x - startInfo.x) < 10 && Math.abs(endInfo.y - startInfo.y) < 10) {
+        return result
+    }
     if (endInfo.x > startInfo.x && endInfo.y <= startInfo.y) { //第一象限(startPostion为原点)
-        result.width = Math.abs(endInfo.x - startInfo.x);
-        result.height = Math.abs(endInfo.y - startInfo.y);
-        result.x = startInfo.x;
-        result.y = endInfo.y;
-        result.lineInfo.startX = 0;
-        result.lineInfo.startY = result.height;
-        result.lineInfo.endX = result.width - arrowWidth;
-        result.lineInfo.endY = 0 + arrowHeight;
         result.lineInfo.cpx = 0;
         result.lineInfo.cpy = 0;
+        if (Math.abs(endInfo.y - startInfo.y) < minHeight) {
+            result.height = minHeight
+            result.y = startInfo.y - minHeight / 2;
+            result.lineInfo.startY = minHeight / 2;
+            result.lineInfo.endY = minHeight / 2;
+            result.lineInfo.cpx = result.lineInfo.endX;
+            result.lineInfo.cpy = result.lineInfo.endY;
+        } else {
+            result.y = endInfo.y;
+            result.height = Math.abs(endInfo.y - startInfo.y);
+            result.lineInfo.startY = result.height;
+            result.lineInfo.endY = arrowWidth;
+        }
+        if (Math.abs(endInfo.x - startInfo.x) < minWidth) {
+            result.lineInfo.endY = 0 + arrowLength;
+            result.width = minWidth;
+            result.x = startInfo.x - minWidth / 2;
+            result.lineInfo.startX = minWidth / 2;
+            result.lineInfo.endX = minWidth / 2;
+            result.lineInfo.arrowP1.x = result.lineInfo.endX - arrowWidth
+            result.lineInfo.arrowP1.y = result.lineInfo.endY
+            result.lineInfo.arrowP2.x = result.lineInfo.endX + arrowWidth
+            result.lineInfo.arrowP2.y = result.lineInfo.endY
+            result.lineInfo.arrowP3.x = result.lineInfo.endX
+            result.lineInfo.arrowP3.y = result.lineInfo.endY - arrowLength
+            result.lineInfo.cpx = result.lineInfo.endX;
+            result.lineInfo.cpy = result.lineInfo.endY;
+        } else {
+            result.width = Math.abs(endInfo.x - startInfo.x);
+            result.x = startInfo.x;
+            result.lineInfo.startX = 0;
+            result.lineInfo.endX = result.width - arrowLength;
+            result.lineInfo.arrowP1.x = result.lineInfo.endX
+            result.lineInfo.arrowP1.y = result.lineInfo.endY - arrowWidth
+            result.lineInfo.arrowP2.x = result.lineInfo.endX
+            result.lineInfo.arrowP2.y = result.lineInfo.endY + arrowWidth
+            result.lineInfo.arrowP3.x = result.lineInfo.endX + arrowLength
+            result.lineInfo.arrowP3.y = result.lineInfo.endY
+        }
     } else if (endInfo.x <= startInfo.x && endInfo.y <= startInfo.y) { //第二象限
-        result.width = Math.abs(endInfo.x - startInfo.x);
-        result.height = Math.abs(endInfo.y - startInfo.y);
-        result.x = endInfo.x;
-        result.y = endInfo.y;
-        result.lineInfo.startX = result.width;
-        result.lineInfo.startY = result.height;
-        result.lineInfo.endX = 0 + arrowWidth;
-        result.lineInfo.endY = 0 + arrowHeight;
-        result.lineInfo.cpx = result.width;
-        result.lineInfo.cpy = 0;
+        if (Math.abs(endInfo.y - startInfo.y) < minHeight) {
+            result.y = startInfo.y - minHeight / 2;
+            result.lineInfo.startY = minHeight / 2;
+            result.lineInfo.endY = minHeight / 2;
+            result.height = minHeight;
+            result.lineInfo.cpx = result.lineInfo.endX;
+            result.lineInfo.cpy = result.lineInfo.endY;
+        } else {
+            result.height = Math.abs(endInfo.y - startInfo.y);
+            result.y = endInfo.y;
+            result.lineInfo.startY = result.height;
+            result.lineInfo.endY = arrowWidth;
+            result.lineInfo.cpx = result.width;
+            result.lineInfo.cpy = 0;
+        }
+        if (Math.abs(endInfo.x - startInfo.x) < minWidth) {
+            result.lineInfo.endY = arrowLength;
+            result.width = minWidth;
+            result.x = startInfo.x - minWidth / 2;
+            result.lineInfo.startX = minWidth / 2;
+            result.lineInfo.endX = minWidth / 2;
+            result.lineInfo.arrowP1.x = result.lineInfo.endX - arrowWidth
+            result.lineInfo.arrowP1.y = result.lineInfo.endY
+            result.lineInfo.arrowP2.x = result.lineInfo.endX + arrowWidth
+            result.lineInfo.arrowP2.y = result.lineInfo.endY
+            result.lineInfo.arrowP3.x = result.lineInfo.endX
+            result.lineInfo.arrowP3.y = result.lineInfo.endY - arrowLength
+            result.lineInfo.cpx = result.lineInfo.endX;
+            result.lineInfo.cpy = result.lineInfo.endY;
+        } else {
+            result.width = Math.abs(endInfo.x - startInfo.x);
+            result.x = endInfo.x;
+            result.lineInfo.startX = result.width;
+            result.lineInfo.endX = 0 + arrowLength;
+            result.lineInfo.arrowP1.x = result.lineInfo.endX
+            result.lineInfo.arrowP1.y = result.lineInfo.endY - arrowWidth
+            result.lineInfo.arrowP2.x = result.lineInfo.endX
+            result.lineInfo.arrowP2.y = result.lineInfo.endY + arrowWidth
+            result.lineInfo.arrowP3.x = result.lineInfo.endX - arrowLength
+            result.lineInfo.arrowP3.y = result.lineInfo.endY
+            result.lineInfo.cpx = result.width;
+            result.lineInfo.cpy = 0;
+        }
     } else if (endInfo.x <= startInfo.x && endInfo.y > startInfo.y) { //第三象限
-        result.width = Math.abs(endInfo.x - startInfo.x);
-        result.height = Math.abs(endInfo.y - startInfo.y);
-        result.x = endInfo.x;
-        result.y = startInfo.y;
-        result.lineInfo.startX = result.width;
-        result.lineInfo.startY = 0;
-        result.lineInfo.endX = 0 + arrowWidth;
-        result.lineInfo.endY = result.height - arrowHeight;
         result.lineInfo.cpx = result.width;
         result.lineInfo.cpy = result.height;
+        if (Math.abs(endInfo.y - startInfo.y) < minHeight) {
+            result.y = startInfo.y - minHeight / 2;
+            result.lineInfo.startY = minHeight / 2;
+            result.lineInfo.endY = minHeight / 2;
+            result.height = minHeight;
+            result.lineInfo.cpx = result.lineInfo.endX;
+            result.lineInfo.cpy = result.lineInfo.endY;
+        } else {
+            result.height = Math.abs(endInfo.y - startInfo.y);
+            result.y = startInfo.y;
+            result.lineInfo.startY = 0;
+            result.lineInfo.endY = result.height - arrowWidth;
+        }
+        if (Math.abs(endInfo.x - startInfo.x) < minWidth) {
+            result.width = minWidth;
+            result.x = startInfo.x - minWidth / 2;
+            result.lineInfo.startX = minWidth / 2;
+            result.lineInfo.endX = minWidth / 2;
+            result.lineInfo.endY = result.height - arrowLength;
+            result.lineInfo.arrowP1.x = result.lineInfo.endX - arrowWidth
+            result.lineInfo.arrowP1.y = result.lineInfo.endY
+            result.lineInfo.arrowP2.x = result.lineInfo.endX + arrowWidth
+            result.lineInfo.arrowP2.y = result.lineInfo.endY
+            result.lineInfo.arrowP3.x = result.lineInfo.endX
+            result.lineInfo.arrowP3.y = result.lineInfo.endY + arrowLength
+            result.lineInfo.cpx = result.lineInfo.endX;
+            result.lineInfo.cpy = result.lineInfo.endY;
+        } else {
+            result.width = Math.abs(endInfo.x - startInfo.x);
+            result.x = endInfo.x;
+            result.lineInfo.startX = result.width;
+            result.lineInfo.endX = 0 + arrowLength;
+            result.lineInfo.arrowP1.x = result.lineInfo.endX
+            result.lineInfo.arrowP1.y = result.lineInfo.endY - arrowWidth
+            result.lineInfo.arrowP2.x = result.lineInfo.endX
+            result.lineInfo.arrowP2.y = result.lineInfo.endY + arrowWidth
+            result.lineInfo.arrowP3.x = result.lineInfo.endX - arrowLength
+            result.lineInfo.arrowP3.y = result.lineInfo.endY
+        }
     } else if (endInfo.x > startInfo.x && endInfo.y > startInfo.y) { //第四象限
-        result.width = Math.abs(endInfo.x - startInfo.x);
-        result.height = Math.abs(endInfo.y - startInfo.y);
-        result.x = startInfo.x;
-        result.y = startInfo.y;
-        result.lineInfo.startX = 0;
-        result.lineInfo.startY = 0;
-        result.lineInfo.endX = result.width - arrowWidth;
-        result.lineInfo.endY = result.height - arrowHeight;
         result.lineInfo.cpx = 0;
         result.lineInfo.cpy = result.height;
+        if (Math.abs(endInfo.y - startInfo.y) < minHeight) {
+            result.y = startInfo.y - minHeight / 2;
+            result.lineInfo.startY = minHeight / 2;
+            result.lineInfo.endY = minHeight / 2;
+            result.height = minHeight;
+            result.lineInfo.cpx = result.lineInfo.endX;
+            result.lineInfo.cpy = result.lineInfo.endY;
+        } else {
+            result.height = Math.abs(endInfo.y - startInfo.y);
+            result.y = startInfo.y;
+            result.lineInfo.startY = 0;
+            result.lineInfo.endY = result.height - arrowWidth;
+        }
+        if (Math.abs(endInfo.x - startInfo.x) < minWidth) {
+            result.width = minWidth;
+            result.x = startInfo.x - minWidth / 2;
+            result.lineInfo.startX = minWidth / 2;
+            result.lineInfo.endX = minWidth / 2;
+            result.lineInfo.endY = result.height - arrowLength;
+            result.lineInfo.arrowP1.x = result.lineInfo.endX - arrowWidth
+            result.lineInfo.arrowP1.y = result.lineInfo.endY
+            result.lineInfo.arrowP2.x = result.lineInfo.endX + arrowWidth
+            result.lineInfo.arrowP2.y = result.lineInfo.endY
+            result.lineInfo.arrowP3.x = result.lineInfo.endX
+            result.lineInfo.arrowP3.y = result.lineInfo.endY + arrowLength
+            result.lineInfo.cpx = result.lineInfo.endX;
+            result.lineInfo.cpy = result.lineInfo.endY;
+        } else {
+            result.width = Math.abs(endInfo.x - startInfo.x);
+            result.x = startInfo.x;
+            result.lineInfo.startX = 0;
+            result.lineInfo.endX = result.width - arrowLength;
+            result.lineInfo.arrowP1.x = result.lineInfo.endX
+            result.lineInfo.arrowP1.y = result.lineInfo.endY - arrowWidth
+            result.lineInfo.arrowP2.x = result.lineInfo.endX
+            result.lineInfo.arrowP2.y = result.lineInfo.endY + arrowWidth
+            result.lineInfo.arrowP3.x = result.lineInfo.endX + arrowLength
+            result.lineInfo.arrowP3.y = result.lineInfo.endY
+        }
     }
     return result;
 }
