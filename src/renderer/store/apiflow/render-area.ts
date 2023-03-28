238efd6d7c7603e0df6a63c2306db61c82be2b4f
shuxiaokai <2581105856@qq.com>
2023-03-28 21:57:33 +0800

    refactor: (apiflow) 新增画布状态

---
+++ src/renderer/store/apiflow/render-area.ts
@@ -0,0 +1,14 @@
+import { FlowRenderArea } from "@@/apiflow";
+import { defineStore } from "pinia";
+
+export const useFlowRenderAreaStore = defineStore("renderArea", {
+    state: (): FlowRenderArea => {
+        return {
+            width: 0,
+            height: 0,
+            offsetX: 0,
+            offsetY: 0,
+            gridUnit: 15,
+        };
+    },
+});
