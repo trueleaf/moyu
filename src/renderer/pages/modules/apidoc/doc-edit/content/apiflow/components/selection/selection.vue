24c0ab457f89c205e7902c38ff02a7b737f65969
shuxiaokai <2581105856@qq.com>
2023-04-11 22:58:48 +0800

    feat: (apiflow) 新增selection选择框

---
+++ src/renderer/pages/modules/apidoc/doc-edit/content/apiflow/components/selection/selection.vue
@@ -0,0 +1,31 @@
+<template>
+    <div
+        v-if="selectionStore.width"
+        class="selection"
+        :style="{
+            left: selectionStore.offsetX + 'px',
+            top: selectionStore.offsetY + 'px',
+            width: selectionStore.width + 'px',
+            height: selectionStore.height + 'px',
+            zIndex: configStore.selectionZIndex
+        }"
+    >
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useFlowConfigStore } from "@/store/apiflow/config";
+import { useFlowSelectionStore } from "@/store/apiflow/selection";
+
+const selectionStore = useFlowSelectionStore()
+const configStore = useFlowConfigStore()
+
+</script>
+
+<style lang="scss" scoped>
+.selection {
+    position: absolute;
+    background-color: rgba(22,145,232,.1);
+    border: 1px solid #6eb1eb;
+}
+</style>
