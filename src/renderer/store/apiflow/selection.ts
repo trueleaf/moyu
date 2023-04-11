1c0b10b658975d19f71b469d0b6e33f88ff94da9
shuxiaokai <2581105856@qq.com>
2023-04-11 22:58:42 +0800

    feat: (apiflow) 新增selection选择框

---
+++ src/renderer/store/apiflow/selection.ts
@@ -0,0 +1,18 @@
+import { FlowSelection } from "@@/apiflow"
+import { defineStore } from "pinia"
+
+export const useFlowSelectionStore = defineStore("flowSelection", {
+    state: (): FlowSelection => {
+        return {
+            isMouseDown: false,
+            startOffsetX: 0,
+            startOffsetY: 0,
+            endOffsetX: 0,
+            endOffsetY: 0,
+            width: 0,
+            height: 0,
+            offsetX: 0,
+            offsetY: 0,
+        }
+    }
+})
