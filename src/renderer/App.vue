93fadf5add4f7a2d74efe760ef59513bc9befee6
shuxiaokai <2581105856@qq.com>
2023-09-12 22:19:42 +0800

    upd: 在初始化地方绑定快捷键

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -8,14 +8,30 @@
   <router-view></router-view>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import { onMounted, onBeforeUnmount } from 'vue'
+import type { IpcRenderer } from 'electron';
 import config from '@/../config/config'
+import { $t } from './i18n/i18n';
 
-export default defineComponent({
-  created() {
-    if (!config.isDev && config.localization.consoleWelcome) {
-      console.log(`
+let ipcRenderer: IpcRenderer;
+if (window.require) {
+  // eslint-disable-next-line prefer-destructuring
+  ipcRenderer = window.require('electron').ipcRenderer;
+}
+const bindShortcut = (e: KeyboardEvent) => {
+  if (e.ctrlKey && (e.key === 'R' || e.key === 'r')) {
+    window.location.reload()
+  } else if (e.ctrlKey && (e.key === 'F5')) {
+    ipcRenderer.send('force-reload')
+  } else if (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'i')) {
+    ipcRenderer.send('open-dev-tools')
+  }
+}
+
+onMounted(() => {
+  if (!config.isDev && config.localization.consoleWelcome) {
+    console.log(`
                      _ _            _ _           _ _ _ _ _ _ _     _ _      _ _    _ _        _ _
                     / _ \\          / _ \\         / _ _ _ _ _ _ \\    \\   \\   /   /   |  |       |  |
                    / / \\ \\        / / \\ \\       / /           \\ \\    \\   \\ /   /    |  |       |  |
@@ -24,21 +40,25 @@ export default defineComponent({
                 / /       \\ \\  / /       \\ \\    \\ \\_ _ _ _ _ _/ /       /   /       \\ _|_ _ _ _| _/
                /_/         \\_\\/_/         \\_\\    \\_ _ _ _ _ _ _/       /_ _/         \\ _ _ _ _ _ /
 
-               ${this.$t('基于Vue和Electron的接口文档工具')}
+               ${$t('基于Vue和Electron的接口文档工具')}
 
-               ${this.$t('GitHub地址')}：https://github.com/trueleaf/moyu
+               ${$t('GitHub地址')}：https://github.com/trueleaf/moyu
 
-               ${this.$t('Gitee地址')}：https://gitee.com/shuzhikai/apiflow
+               ${$t('Gitee地址')}：https://gitee.com/shuzhikai/apiflow
 
-               ${this.$t('最近一次更新')}：${process.env.VUE_APP_BUILD_TIME}
+               ${$t('最近一次更新')}：${process.env.VUE_APP_BUILD_TIME}
             `)
-    }
-    if (!config.localization.consoleWelcome) {
-      console.log(process.env.VUE_APP_BUILD_TIME);
-    }
-    document.title = `${config.isDev ? `${config.localization.title}(本地)` : config.localization.title} `;
-  },
+  }
+  if (!config.localization.consoleWelcome) {
+    console.log(process.env.VUE_APP_BUILD_TIME);
+  }
+  document.title = `${config.isDev ? `${config.localization.title}(本地)` : config.localization.title} `;
+  window.addEventListener('keydown', bindShortcut);
 })
+onBeforeUnmount(() => {
+  window.removeEventListener('keydown', bindShortcut);
+})
+
 </script>
 
 <style lang="scss">
