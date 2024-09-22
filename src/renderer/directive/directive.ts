853c6ca921d9ebad3642c6d418ef1d1df4d239bc
舒小凯 <2581105856@qq.com>
2024-09-22 11:22:03 +0800

    refactor: 更新自定义指令使用

--- src/renderer/directive/directive.ts
+++ src/renderer/directive/directive.ts
@@ -1,5 +1,4 @@
 import { App, DirectiveBinding } from 'vue'
-// import scssData from '@/scss/variables/_variables.scss'
 
 let domList: HTMLElement[] = [];
 
@@ -18,164 +17,166 @@ function createTipDom(left: number, top: number): HTMLElement {
   return tipDom
 }
 
-export default (app: App): void => {
-  //=====================================成功提示指令====================================//
-  app.directive('success', {
-    unmounted() {
-      domList = [];
-    },
-    updated(el: Element, binding) {
-      if (binding.value && !binding.oldValue) {
-        const { left, top } = el.getBoundingClientRect();
-        const tipDom = createTipDom(left, top);
-        setTimeout(() => {
-          tipDom.style.transition = 'transform .5s';
-          tipDom.style.transform = 'translate3D(0, -30px, 0)';
-        })
-        setTimeout(() => {
-          document.body.removeChild(tipDom);
-        }, 500)
-      }
-    },
-  });
-  //=====================================flex指令====================================//
-  app.directive('flex1', {
-    mounted(el: HTMLElement, binding) {
-      setTimeout(() => {
-        const offsetY = el.getBoundingClientRect().y;
-        const { value } = binding;
-        el.style.height = `calc(100vh - ${offsetY + value}px)`;
-        el.style.overflowY = 'auto';
-      });
-    },
-    updated(el: HTMLElement, binding) {
-      setTimeout(() => {
-        const offsetY = el.getBoundingClientRect().y;
-        const { value } = binding;
-        el.style.height = `calc(100vh - ${offsetY + value}px)`;
-        el.style.overflowY = 'auto';
-      });
-    },
-  });
-  //=====================================拷贝指令====================================//
-  // const copyValue = ref("");
-  const runCopy = (e: MouseEvent, el: HTMLElement) => {
-    const x = e.clientX;
-    const y = e.clientY;
-    const dom = document.createElement('textarea');
-    dom.value = el.dataset.value || '';
-    dom.style.position = 'fixed';
-    dom.style.top = '-9999px';
-    dom.style.left = '-9999px';
-    document.body.appendChild(dom);
-    dom.select();
-    document.execCommand('Copy', false);
-    document.body.removeChild(dom);
-    //提示
-    const span = document.createElement('span');
-    span.innerHTML = '复制成功';
-    span.style.transition = 'all .6s';
-    span.style.color = '#2c2';
-    span.style.position = 'fixed';
-    span.style.left = `${x}px`;
-    span.style.top = `${y}px`;
-    span.style.whiteSpace = 'nowrap';
-    // span.style.zIndex = scssData.zIndexCopy;
-    span.style.transform = 'translate3D(0, -1em, 0)';
-    document.documentElement.appendChild(span);
-    requestAnimationFrame(() => {
-      span.style.transform = 'translate3D(0, -2.5em, 0)';
+export const customDirective = {
+  install(app: App) {
+    //=====================================成功提示指令====================================//
+    app.directive('success', {
+      unmounted() {
+        domList = [];
+      },
+      updated(el: Element, binding) {
+        if (binding.value && !binding.oldValue) {
+          const { left, top } = el.getBoundingClientRect();
+          const tipDom = createTipDom(left, top);
+          setTimeout(() => {
+            tipDom.style.transition = 'transform .5s';
+            tipDom.style.transform = 'translate3D(0, -30px, 0)';
+          })
+          setTimeout(() => {
+            document.body.removeChild(tipDom);
+          }, 500)
+        }
+      },
     });
-    setTimeout(() => {
-      document.documentElement.removeChild(span);
-    }, 800);
-  };
-  let bindCopyFn: ((e: MouseEvent) => void) | null = null;
-  app.directive('copy', {
-    mounted(el: HTMLElement, binding) {
-      // copyValue.value = binding.value;
-      el.dataset.value = binding.value
-      bindCopyFn = (e: MouseEvent) => {
-        runCopy(e, el);
-      }
-      el.addEventListener('click', bindCopyFn)
-    },
-    updated(el: HTMLElement, binding) {
-      // copyValue.value = binding.value;
-      el.dataset.value = binding.value
-    },
-    unmounted(el: HTMLElement) {
-      if (bindCopyFn) {
-        el.removeEventListener('click', bindCopyFn)
-      }
-    }
-  })
-  //=====================================倒计时指令====================================//
-  const countdownTimers: number[] = [];
-  const countdown = (el: HTMLElement, binding: DirectiveBinding<number>) => {
-    let restTime = (binding.value - Date.now()) > 0 ? (binding.value - Date.now()) : 0;
-    if (restTime === 0) {
-      el.innerHTML = '已过期';
-      countdownTimers.forEach(t => {
-        clearInterval(t);
-      })
-      return;
-    }
-    const hasFullDay = restTime > 86400000;
-    const day = hasFullDay ? Math.floor(restTime / 86400000) : 0;
-    if (hasFullDay) {
-      restTime %= 86400000
-    }
-    const hasFullHour = restTime > 3600000;
-    const hour = hasFullHour ? Math.floor(restTime / 3600000) : 0;
-    if (hasFullHour) {
-      restTime %= 3600000;
-    }
-    const hasFullMinute = restTime > 60000;
-    const minute = hasFullMinute ? Math.floor(restTime / 60000) : 0;
-    if (hasFullMinute) {
-      restTime %= 60000;
-    }
-    const second = Math.floor(restTime / 1000);
-    el.innerHTML = `${day}天${hour}小时${minute}分${second}秒`;
-  }
-  const bindCountdown = (el: HTMLElement, binding: DirectiveBinding<number>) => {
-    countdown(el, binding);
-    const timer = window.setInterval(() => {
-      countdown(el, binding);
-    }, 1000)
-
-    countdownTimers.push(timer);
-  }
-  app.directive('countdown', {
-    mounted(el: HTMLElement, binding) {
-      bindCountdown(el, binding);
-    },
-    updated(el: HTMLElement, binding) {
-      countdownTimers.forEach(t => {
-        clearInterval(t);
-      })
-      bindCountdown(el, binding);
-    },
-    unmounted() {
-      countdownTimers.forEach(t => {
-        clearInterval(t);
-      })
-    }
-  })
-  //=====================================focus指令====================================//
-  app.directive('focus-select', { //如果当前元素不是input则递归查找
-    mounted(el: HTMLElement) {
-      if (el.tagName === 'INPUT') {
-        el.focus();
-        (el as HTMLInputElement).select();
-      } else {
-        const ipt = el.querySelector('input');
+    //=====================================flex指令====================================//
+    app.directive('flex1', {
+      mounted(el: HTMLElement, binding) {
         setTimeout(() => {
-                    ipt?.select()
-                    ipt?.focus()
+          const offsetY = el.getBoundingClientRect().y;
+          const { value } = binding;
+          el.style.height = `calc(100vh - ${offsetY + value}px)`;
+          el.style.overflowY = 'auto';
         });
+      },
+      updated(el: HTMLElement, binding) {
+        setTimeout(() => {
+          const offsetY = el.getBoundingClientRect().y;
+          const { value } = binding;
+          el.style.height = `calc(100vh - ${offsetY + value}px)`;
+          el.style.overflowY = 'auto';
+        });
+      },
+    });
+    //=====================================拷贝指令====================================//
+    // const copyValue = ref("");
+    const runCopy = (e: MouseEvent, el: HTMLElement) => {
+      const x = e.clientX;
+      const y = e.clientY;
+      const dom = document.createElement('textarea');
+      dom.value = el.dataset.value || '';
+      dom.style.position = 'fixed';
+      dom.style.top = '-9999px';
+      dom.style.left = '-9999px';
+      document.body.appendChild(dom);
+      dom.select();
+      document.execCommand('Copy', false);
+      document.body.removeChild(dom);
+      //提示
+      const span = document.createElement('span');
+      span.innerHTML = '复制成功';
+      span.style.transition = 'all .6s';
+      span.style.color = '#2c2';
+      span.style.position = 'fixed';
+      span.style.left = `${x}px`;
+      span.style.top = `${y}px`;
+      span.style.whiteSpace = 'nowrap';
+      // span.style.zIndex = scssData.zIndexCopy;
+      span.style.transform = 'translate3D(0, -1em, 0)';
+      document.documentElement.appendChild(span);
+      requestAnimationFrame(() => {
+        span.style.transform = 'translate3D(0, -2.5em, 0)';
+      });
+      setTimeout(() => {
+        document.documentElement.removeChild(span);
+      }, 800);
+    };
+    let bindCopyFn: ((e: MouseEvent) => void) | null = null;
+    app.directive('copy', {
+      mounted(el: HTMLElement, binding) {
+        // copyValue.value = binding.value;
+        el.dataset.value = binding.value
+        bindCopyFn = (e: MouseEvent) => {
+          runCopy(e, el);
+        }
+        el.addEventListener('click', bindCopyFn)
+      },
+      updated(el: HTMLElement, binding) {
+        // copyValue.value = binding.value;
+        el.dataset.value = binding.value
+      },
+      unmounted(el: HTMLElement) {
+        if (bindCopyFn) {
+          el.removeEventListener('click', bindCopyFn)
+        }
       }
-    },
-  })
+    })
+    //=====================================倒计时指令====================================//
+    const countdownTimers: number[] = [];
+    const countdown = (el: HTMLElement, binding: DirectiveBinding<number>) => {
+      let restTime = (binding.value - Date.now()) > 0 ? (binding.value - Date.now()) : 0;
+      if (restTime === 0) {
+        el.innerHTML = '已过期';
+        countdownTimers.forEach(t => {
+          clearInterval(t);
+        })
+        return;
+      }
+      const hasFullDay = restTime > 86400000;
+      const day = hasFullDay ? Math.floor(restTime / 86400000) : 0;
+      if (hasFullDay) {
+        restTime %= 86400000
+      }
+      const hasFullHour = restTime > 3600000;
+      const hour = hasFullHour ? Math.floor(restTime / 3600000) : 0;
+      if (hasFullHour) {
+        restTime %= 3600000;
+      }
+      const hasFullMinute = restTime > 60000;
+      const minute = hasFullMinute ? Math.floor(restTime / 60000) : 0;
+      if (hasFullMinute) {
+        restTime %= 60000;
+      }
+      const second = Math.floor(restTime / 1000);
+      el.innerHTML = `${day}天${hour}小时${minute}分${second}秒`;
+    }
+    const bindCountdown = (el: HTMLElement, binding: DirectiveBinding<number>) => {
+      countdown(el, binding);
+      const timer = window.setInterval(() => {
+        countdown(el, binding);
+      }, 1000)
+
+      countdownTimers.push(timer);
+    }
+    app.directive('countdown', {
+      mounted(el: HTMLElement, binding) {
+        bindCountdown(el, binding);
+      },
+      updated(el: HTMLElement, binding) {
+        countdownTimers.forEach(t => {
+          clearInterval(t);
+        })
+        bindCountdown(el, binding);
+      },
+      unmounted() {
+        countdownTimers.forEach(t => {
+          clearInterval(t);
+        })
+      }
+    })
+    //=====================================focus指令====================================//
+    app.directive('focus-select', { //如果当前元素不是input则递归查找
+      mounted(el: HTMLElement) {
+        if (el.tagName === 'INPUT') {
+          el.focus();
+          (el as HTMLInputElement).select();
+        } else {
+          const ipt = el.querySelector('input');
+          setTimeout(() => {
+            ipt?.select()
+            ipt?.focus()
+          });
+        }
+      },
+    })
+  }
 }
--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -9,6 +9,7 @@ import '@/assets/css/index.css'
 import i18next from 'i18next';
 import I18NextVue from "i18next-vue";
 import Backend from 'i18next-http-backend'
+import { customDirective } from './directive/directive';
 
 
 const pinia = createPinia();
@@ -20,5 +21,5 @@ await i18next.use(Backend).init({
   fallbackLng: "zh",
 });
 app.use(pinia)
-app.use(axiosPlugin).use(I18NextVue, { i18next }).use(ElementPlus, { locale: zhCn }).use(router);
+app.use(axiosPlugin).use(customDirective).use(I18NextVue, { i18next }).use(ElementPlus, { locale: zhCn }).use(router);
 app.mount('#app')
