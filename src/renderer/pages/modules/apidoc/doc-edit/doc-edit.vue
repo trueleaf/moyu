c6944f2d5a6a8131fd113a7af72b2c66c8830752
舒小凯 <2581105856@qq.com>
2024-09-25 10:36:37 +0800

    feat: 重构文档编辑页面，优化代码结构

--- src/renderer/pages/modules/apidoc/doc-edit/doc-edit.vue
+++ src/renderer/pages/modules/apidoc/doc-edit/doc-edit.vue
@@ -1,49 +1,50 @@
-/*
-    创建者：shuxiaokai
-    创建时间：2021-07-21 21:58
-    模块名称：文档编辑页面
-    备注：
-*/
 <template>
   <div class="doc-view">
-    <s-banner></s-banner>
+    <Banner></Banner>
     <div class="doc-wrap">
-      <s-nav></s-nav>
-      <s-content></s-content>
+      <Nav></Nav>
+      <!-- <Content></Content> -->
     </div>
-    <s-save-doc-dialog v-if="saveDocDialogVisible" v-model="saveDocDialogVisible"></s-save-doc-dialog>
+    <SaveDocDialog v-if="saveDocDialogVisible" v-model="saveDocDialogVisible"></SaveDocDialog>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { computed, onMounted, onBeforeUnmount } from 'vue'
-import { router } from '@/router/index'
-import { useStore } from '@/store/index'
 import { apidocCache } from '@/cache/apidoc'
-import sSaveDocDialog from '@/pages/modules/apidoc/doc-edit/dialog/save-doc/save-doc.vue'
-import sBanner from './banner/banner.vue';
-import sNav from './nav/nav.vue';
-import sContent from './content/content.vue';
+import SaveDocDialog from '@/pages/modules/apidoc/doc-edit/dialog/save-doc/save-doc.vue'
+import Banner from './banner/banner.vue';
+import Nav from './nav/nav.vue';
+// import Content from './content/content.vue';
+import { useApidocTas } from '@/store/apidoc/tabs'
+import { useApidoc } from '@/store/apidoc/apidoc'
+import { useApidocBaseInfo } from '@/store/apidoc/base-info'
+import { useApidocWorkerState } from '@/store/apidoc/worker-state'
+import { useRoute } from 'vue-router';
 
-const store = useStore();
-const projectId = router.currentRoute.value.query.id as string;
+const route = useRoute();
+const apidocTabsStore = useApidocTas();
+const apidocStore = useApidoc()
+const apidocBaseInfoStroe = useApidocBaseInfo();
+const apidocWorkerStateStore = useApidocWorkerState()
+const projectId = route.query.id as string;
 //当前选中的tab
 const currentSelectTab = computed(() => {
-  const tabs = store.state['apidoc/tabs'].tabs[projectId];
-  const selectedTab = tabs?.find((tab) => tab.selected) || null;
+  const currentTabs = apidocTabsStore.tabs[projectId];
+  const selectedTab = currentTabs?.find((tab) => tab.selected) || null;
   return selectedTab;
 })
 //是否正在保存数据
-const saveDocLoading = computed(() => store.state['apidoc/apidoc'].loading)
+const saveDocLoading = computed(() => apidocStore.loading);
 //当前工作区状态
-const isView = computed(() => store.state['apidoc/baseInfo'].mode === 'view')
+const isView = computed(() => apidocBaseInfoStroe.mode === 'view')
 const saveDocDialogVisible = computed({
   get() {
-    return store.state['apidoc/apidoc'].saveDocDialogVisible;
+    return apidocStore.saveDocDialogVisible;
   },
   set(val) {
-    store.commit('apidoc/apidoc/changeSaveDocDialogVisible', val);
-    store.commit('apidoc/apidoc/changeSavedDocId', currentSelectTab.value?._id);
+    apidocStore.changeSaveDocDialogVisible(val);
+    apidocStore.changeSavedDocId(currentSelectTab.value?._id || '');
   }
 });
 //=====================================绑定快捷键====================================//
@@ -51,49 +52,46 @@ const bindShortcut = (e: KeyboardEvent) => {
   if (isView.value) {
     return;
   }
-  const tabs = store.state['apidoc/tabs'].tabs[projectId];
-  const hasTabs = tabs && tabs.length > 0;
+  const currentTabs = apidocTabsStore.tabs[projectId];
+  const hasTabs = currentTabs && currentTabs.length > 0;
   const currentTabIsDoc = currentSelectTab.value?.tabType === 'doc';
   if (hasTabs && currentTabIsDoc && e.ctrlKey && (e.key === 'S' || e.key === 's') && saveDocLoading.value === false) {
     e.preventDefault();
     e.stopPropagation();
     if (currentSelectTab.value._id.includes('local_')) {
       saveDocDialogVisible.value = true
-    } else if (!store.state['apidoc/apidoc'].saveLoading) {
-      store.dispatch('apidoc/apidoc/saveApidoc');
+    } else if (!apidocStore.saveLoading) {
+      apidocStore.saveApidoc();
     }
   } else if (hasTabs && currentTabIsDoc && e.ctrlKey && (e.key === 'W' || e.key === 'w')) {
-    const selectedTab = tabs.find(tab => tab.selected)
+    const selectedTab = currentTabs.find(tab => tab.selected)
     if (selectedTab) {
-      store.dispatch('apidoc/tabs/deleteTabByIds', {
-        projectId,
-        ids: [selectedTab._id]
-      });
+      apidocTabsStore.deleteTabByIds({ projectId, ids: [selectedTab._id] });
     }
   }
 }
 //=====================================基本数据获取====================================//
 //获取项目基本信息
 const getProjectInfo = () => {
-  store.dispatch('apidoc/baseInfo/getProjectBaseInfo', { projectId });
+  apidocBaseInfoStroe.getProjectBaseInfo({ projectId });
 }
 //初始化cookie
 const initCookies = () => {
-  store.commit('apidoc/baseInfo/initCookies')
+  apidocBaseInfoStroe.initCookies()
 }
 //初始化布局
 const initLayout = () => {
-  store.commit('apidoc/baseInfo/initLayout')
+  apidocBaseInfoStroe.initLayout()
 }
 //初始化header信息
 const initCommonHeaders = () => {
-  store.dispatch('apidoc/baseInfo/getCommonHeaders')
+  apidocBaseInfoStroe.getCommonHeaders()
 }
 //初始化worker本地状态
 const initWorkerLocalState = () => {
   const localState = apidocCache.getApidocWorkerLocalStateById(projectId);
   if (localState) {
-    store.commit('apidoc/workerState/changeLocalState', { projectId, value: localState })
+    apidocWorkerStateStore.changeLocalState({ projectId, value: localState });
   }
 }
 onMounted(() => {
@@ -108,24 +106,25 @@ onBeforeUnmount(() => {
   window.removeEventListener('keydown', bindShortcut);
 })
 //初始化预览模式或者编辑模式
-const routerMode = router.currentRoute.value.query.mode as string;
-let mode = 'view';
+const routerMode = route.query.mode as string;
+let mode: "view" | "edit" = 'view';
 if (routerMode === 'view') {
   mode = 'view'
 } else if (routerMode === 'edit') {
   mode = 'edit'
 }
-store.commit('apidoc/baseInfo/changeMode', mode);
+apidocBaseInfoStroe.changeMode(mode);
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .doc-view {
-    display: flex;
+  display: flex;
+  overflow: hidden;
+  height: 100%;
+
+  .doc-wrap {
+    flex: 1;
     overflow: hidden;
-    height: 100%;
-    .doc-wrap {
-        flex: 1;
-        overflow: hidden;
-    }
+  }
 }
 </style>
