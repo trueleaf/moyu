7c44b0a7f0412b8adaf8567601fe882bad64cddf
舒小凯 <2581105856@qq.com>
2024-09-26 13:10:26 +0800

    refactor: 重构monaco-editor依赖优化代码结构

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,15 @@ export default defineConfig(() => {
           additionalData: `@import "./src/renderer/scss/index.scss";`
         }
       }
+    },
+    optimizeDeps: {
+      include: [
+        `monaco-editor/esm/vs/language/json/json.worker`,
+        `monaco-editor/esm/vs/language/css/css.worker`,
+        `monaco-editor/esm/vs/language/html/html.worker`,
+        `monaco-editor/esm/vs/language/typescript/ts.worker`,
+        `monaco-editor/esm/vs/editor/editor.worker`
+      ]
     }
   }
 })
