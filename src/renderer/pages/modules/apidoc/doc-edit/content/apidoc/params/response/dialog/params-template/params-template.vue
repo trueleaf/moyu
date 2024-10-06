0876721d5a73ebee8989690f95b764d48b74f189
舒小凯 <2581105856@qq.com>
2024-10-06 14:51:26 +0800

    refactor: 重构参数模板对话框，优化代码结构

--- src/renderer/pages/modules/apidoc/doc-edit/content/apidoc/params/response/dialog/params-template/params-template.vue
+++ src/renderer/pages/modules/apidoc/doc-edit/content/apidoc/params/response/dialog/params-template/params-template.vue
@@ -1,84 +1,82 @@
-/*
-    创建者：shuxiaokai
-    创建时间：2021-08-29 10:49
-    模块名称：保存参数为模板
-    备注：
-*/
 <template>
-  <s-dialog :model-value="modelValue" width="30%" :title="t('保存参数为模板')" @close="handleClose">
-    <s-form ref="form">
-      <s-form-item label="模板名称" prop="name" one-line required></s-form-item>
-    </s-form>
+  <SDialog :model-value="modelValue" width="30%" :title="t('保存参数为模板')" @close="handleClose">
+    <SForm ref="form">
+      <SFormItem label="模板名称" prop="name" one-line required></SFormItem>
+    </SForm>
     <template #footer>
       <el-button :loading="loading" type="primary" @click="handleSave">{{ t("保存") }}</el-button>
       <el-button type="warning" @click="handleClose">{{ t("取消") }}</el-button>
     </template>
-  </s-dialog>
+  </SDialog>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import SDialog from '@/components/common/dialog/g-dialog.vue'
+import SForm from '@/components/common/forms/form/g-form.vue'
+import SFormItem from '@/components/common/forms/form/g-form-item.vue'
+import { t } from 'i18next'
+import { nextTick, ref } from 'vue';
+import { FormInstance } from 'element-plus';
+import { useApidoc } from '@/store/apidoc/apidoc';
+import { useRoute } from 'vue-router';
+import { axios } from '@/api/api';
+import { useApidocBaseInfo } from '@/store/apidoc/base-info';
 
-export default defineComponent({
-  props: {
-    modelValue: {
-      type: Boolean,
-      default: false,
-    },
-    index: {
-      type: Number,
-      default: 0,
-    },
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
   },
-  emits: ['update:modelValue'],
-  data() {
-    return {
-      loading: false
-    };
-  },
-  methods: {
-    //保存
-    handleSave() {
-      this.$refs.form.validate((valid) => {
-        if (valid) {
-          const { formInfo } = this.$refs.form;
-          const responseParam = this.$store.state['apidoc/apidoc'].apidoc.item.responseParams[this.index];
-          if (!responseParam) {
-            return
-          }
-          const params = {
-            name: formInfo.name,
-            presetParamsType: 'responseParams',
-            projectId: this.$route.query.id,
-            items: responseParam.value.strJson,
-          };
-          this.loading = true;
-          this.axios.post('/api/project/doc_preset_params', params).then((res) => {
-            this.$store.commit('apidoc/baseInfo/addParamsTemplate', res.data);
-            this.handleClose();
-          }).catch((err) => {
-            console.error(err);
-          }).finally(() => {
-            this.loading = false;
-          });
-        } else {
-          this.$nextTick(() => {
-            const input = document.querySelector('.el-form-item.is-error input');
-            if (input) {
-              (input as HTMLElement).focus();
-            }
-          });
-        }
-      });
-    },
-    //关闭弹窗
-    handleClose() {
-      this.$emit('update:modelValue', false);
-    },
+  index: {
+    type: Number,
+    default: 0,
   },
 })
+const emit = defineEmits(['update:modelValue'])
+const loading = ref(false)
+const form = ref<FormInstance>()
+const apidocStore = useApidoc()
+const apidocBaseInfoStore = useApidocBaseInfo()
+const route = useRoute()
+/*
+|--------------------------------------------------------------------------
+| 方法定义
+|--------------------------------------------------------------------------
+*/
+const handleSave = () => {
+ form.value?.validate((valid) => {
+    if (valid) {
+      const { formInfo } = form.value as any;
+      const responseParam =apidocStore.apidoc.item.responseParams[props.index];
+      if (!responseParam) {
+        return
+      }
+      const params = {
+        name: formInfo.name,
+        presetParamsType: 'responseParams',
+        projectId: route.query.id,
+        items: responseParam.value.strJson,
+      };
+      loading.value = true;
+      axios.post('/api/project/doc_preset_params', params).then((res) => {
+        apidocBaseInfoStore.addParamsTemplate(res.data);
+        handleClose();
+      }).catch((err) => {
+        console.error(err);
+      }).finally(() => {
+        loading.value = false;
+      });
+    } else {
+      nextTick(() => {
+        const input = document.querySelector('.el-form-item.is-error input');
+        if (input) {
+          (input as HTMLElement).focus();
+        }
+      });
+    }
+  });
+}
+const handleClose = () => {
+  emit('update:modelValue', false)
+}
 </script>
-
-<style lang="scss" scoped>
-
-</style>
