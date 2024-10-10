2db6f652437f7ae88cc16b631f81033e858b5523
舒小凯 <2581105856@qq.com>
2024-10-10 13:01:24 +0800

    refactor: 重构开发者工具字体设置，优化代码结构

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,7 +3,33 @@ import { app, BrowserWindow, ipcMain } from 'electron'
 import path from 'path'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
-function createWindow () {
+const changeDevtoolsFont = (win: BrowserWindow) => {
+  win.webContents.on('devtools-opened', () => {
+    const css = `
+    :root {
+        --sys-color-base: var(--ref-palette-neutral100);
+        --source-code-font-family: consolas;
+        --source-code-font-size: 12px;
+        --monospace-font-family: consolas;
+        --monospace-font-size: 12px;
+        --default-font-family: system-ui, sans-serif;
+        --default-font-size: 12px;
+    }
+    .-theme-with-dark-background {
+        --sys-color-base: var(--ref-palette-secondary25);
+    }
+    body {
+        --default-font-family: system-ui,sans-serif;
+    }`;
+    win.webContents.devToolsWebContents?.executeJavaScript(`
+    const overriddenStyle = document.createElement('style');
+    overriddenStyle.innerHTML = '${css.replaceAll('\n', ' ')}';
+    document.body.append(overriddenStyle);
+    document.body.classList.remove('platform-windows');`);
+  });
+}
+
+function createWindow() {
   const mainWindow = new BrowserWindow({
     title: `${isDevelopment ? `${config.localization.title}(本地)` : config.localization.title} `,
     webPreferences: {
@@ -13,6 +39,7 @@ function createWindow () {
   })
   mainWindow.loadURL('http://localhost:3000')
   mainWindow.webContents.openDevTools()
+  changeDevtoolsFont(mainWindow);
 }
 
 app.whenReady().then(() => {
@@ -22,7 +49,7 @@ app.whenReady().then(() => {
   ipcMain.handle('open-dev-tools', () => {
     BrowserWindow.getAllWindows()?.forEach(win => {
       win.webContents.openDevTools()
-    }) 
+    })
   })
   createWindow()
 
