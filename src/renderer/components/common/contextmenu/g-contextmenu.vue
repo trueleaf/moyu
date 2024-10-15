a5936b017d390de7785ed9131c7ce12233499625
舒小凯 <2581105856@qq.com>
2024-10-15 12:47:01 +0800

    feat: 重构上下文菜单组件，优化代码结构和样式，支持动态调整位置

--- src/renderer/components/common/contextmenu/g-contextmenu.vue
+++ src/renderer/components/common/contextmenu/g-contextmenu.vue
@@ -1,98 +1,79 @@
-/*
-    创建者：shuxiaokai
-    创建时间：2021-07-30 22:39
-    模块名称：contextmenu
-    备注：
-*/
+
 <template>
-  <div ref="contextmenu" class="s-contextmenu" :style="{width: width, left: left + 'px', top: realTop + 'px'}">
+  <div ref="contextmenu" class="s-contextmenu" :style="{ width: width, left: left + 'px', top: realTop + 'px' }">
     <slot></slot>
   </div>
   <!-- <div class="contextmenu-shadow"></div> -->
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import { ref, watch } from 'vue';
 
-export default defineComponent({
-  props: {
-    /**
-         * 默认宽度
-         */
-    width: {
-      type: String,
-      default: '200px',
-    },
-    /**
-         * 左侧距离
-         */
-    left: {
-      type: Number,
-      default: 0
-    },
-    /**
-         * 顶部距离
-         */
-    top: {
-      type: Number,
-      default: 0
-    },
+
+const props = defineProps({
+  width: {
+    type: String,
+    default: '200px',
   },
-  data() {
-    return {
-      realTop: 0,
-    };
+  left: {
+    type: Number,
+    default: 0
   },
-  watch: {
-    top: {
-      handler(topVal: number) {
-        setTimeout(() => { //保证dom加载完毕
-          const contextmenuDom = this.$refs.contextmenu as HTMLElement;
-          const { innerHeight } = window;
-          const { height } = contextmenuDom.getBoundingClientRect();
-          const contextPosition = height + topVal > innerHeight ? 'top' : 'bottom';
-          if (contextPosition === 'top' && height > topVal) { //显示在上面但是contextmenu高度小于上面可用空间高度
-            contextmenuDom.style.height = `${topVal}px`;
-            contextmenuDom.style.overflowY = 'auto';
-          } else if (contextPosition === 'top') {
-            this.realTop = topVal - height;
-          } else {
-            this.realTop = topVal;
-          }
-        }, 0)
-      },
-      immediate: true,
-    },
+  top: {
+    type: Number,
+    default: 0
   },
-  methods: {
-  },
+})
+const realTop = ref(0);
+const contextmenu = ref<HTMLDivElement>()
+watch(() => props.top, (topVal) => {
+  setTimeout(() => { //保证dom加载完毕
+    const { innerHeight } = window;
+    const { height } = contextmenu.value!.getBoundingClientRect();
+    const contextPosition = height + topVal > innerHeight ? 'top' : 'bottom';
+    if (contextPosition === 'top' && height > topVal) { //显示在上面但是contextmenu高度小于上面可用空间高度
+      contextmenu.value!.style.height = `${topVal}px`;
+      contextmenu.value!.style.overflowY = 'auto';
+    } else if (contextPosition === 'top') {
+      realTop.value = topVal - height;
+    } else {
+      realTop.value = topVal;
+    }
+  }, 0)
+}, {
+  immediate: true
 })
 </script>
 
 <style lang="scss" scoped>
 .s-contextmenu {
-    position: fixed;
-    background: $white;
-    box-shadow: rgb(0 0 0 / 10%) 0px 2px 8px 0px; //墨刀弹窗样式
-    z-index: $zIndex-contextmenu;
-    animation: ctx-fade .2s;
-    &::-webkit-scrollbar {
-        width: size(5);
+  position: fixed;
+  background: $white;
+  box-shadow: rgb(0 0 0 / 10%) 0px 2px 8px 0px; //墨刀弹窗样式
+  z-index: $zIndex-contextmenu;
+  animation: ctx-fade .2s;
+
+  &::-webkit-scrollbar {
+    width: size(5);
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: $gray-400;
+  }
+
+  @keyframes ctx-fade {
+    from {
+      // transform: scale(0.8);
+      opacity: 0;
     }
-    &::-webkit-scrollbar-thumb {
-        background: $gray-400;
+
+    to {
+      // transform: scale(1);
+      opacity: 1;
     }
-    @keyframes ctx-fade {
-        from {
-            // transform: scale(0.8);
-            opacity: 0;
-        }
-        to {
-            // transform: scale(1);
-            opacity: 1;
-        }
-    }
+  }
 }
+
 // .contextmenu-shadow {
 //     position: fixed;
 //     left: 0;
@@ -101,5 +82,4 @@ export default defineComponent({
 //     bottom: 0;
 //     background: rgba(0, 0, 0, 0);
 //     z-index: $zIndex-contextmenu - 1;
-// }
-</style>
+// }</style>
