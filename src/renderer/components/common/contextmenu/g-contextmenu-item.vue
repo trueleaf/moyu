acaf27c2ad2ff306e694440a0514edc7f9056bdb
舒小凯 <2581105856@qq.com>
2024-10-15 12:46:54 +0800

    refactor: 重构contextmenu-item组件，优化代码结构和样式

--- src/renderer/components/common/contextmenu/g-contextmenu-item.vue
+++ src/renderer/components/common/contextmenu/g-contextmenu-item.vue
@@ -1,92 +1,72 @@
-/*
-    创建者：shuxiaokai
-    创建时间：2021-07-30 22:39
-    模块名称：contextmenu-item
-    备注：
-*/
 <template>
   <div v-if="type === 'divider'" class="s-contextmenu-divider"></div>
-  <div v-else class="s-contextmenu-item" :class="{disabled: disabled}" @click="handleClickItem">
+  <div v-else class="s-contextmenu-item" :class="{ disabled: disabled }" @click="handleClickItem">
     <span>{{ label }}</span>
     <span class="hot-key">{{ hotKey }}</span>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType } from 'vue'
+<script lang="ts" setup>
+import { PropType } from 'vue'
 
-export default defineComponent({
-  props: {
-    /**
-         * 标签
-         */
-    label: {
-      type: String,
-      default: '',
-    },
-    /**
-         * 快捷键
-         */
-    hotKey: {
-      type: String,
-      default: '',
-    },
-    /**
-         * 类型，divider代表分割线
-         */
-    type: {
-      type: String as PropType<'divider' | ''>,
-      default: '',
-    },
-    /**
-         * 是否禁用
-         */
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
+const props = defineProps({
+  label: {
+    type: String,
+    default: '',
   },
-  emits: ['click'],
-  data() {
-    return {
-    };
+  hotKey: {
+    type: String,
+    default: '',
   },
-  methods: {
-    handleClickItem(e: MouseEvent) {
-      if (this.disabled) {
-        return;
-      }
-      this.$emits('click', e)
-    },
+  type: {
+    type: String as PropType<'divider' | ''>,
+    default: '',
   },
+  disabled: {
+    type: Boolean,
+    default: false,
+  }
 })
+const emits = defineEmits(['click'])
+const handleClickItem = (e: MouseEvent) => {
+  if (props.disabled) {
+    return;
+  }
+  emits('click', e)
+}
+
 </script>
 
 <style lang="scss" scoped>
 .s-contextmenu-item {
-    line-height: 1.8em;
-    padding: size(5) size(25);
-    cursor: pointer;
-    display: flex;
-    &.disabled {
-        color: $gray-400;
-        cursor: default;
-        &:hover {
-            background: inherit;
-            color: $gray-400;
-        }
-    }
-    .hot-key {
-        margin-left: auto;
-        color: $gray-500;
-    }
+  line-height: 1.8em;
+  padding: size(5) size(25);
+  cursor: pointer;
+  display: flex;
+
+  &.disabled {
+    color: $gray-400;
+    cursor: default;
+
     &:hover {
-        background: $gray-200;
-        color: $theme-color;
+      background: inherit;
+      color: $gray-400;
     }
+  }
+
+  .hot-key {
+    margin-left: auto;
+    color: $gray-500;
+  }
+
+  &:hover {
+    background: $gray-200;
+    color: $theme-color;
+  }
 }
+
 .s-contextmenu-divider {
-    margin: size(4) 0;
-    border-top: 1px solid $gray-200;
+  margin: size(4) 0;
+  border-top: 1px solid $gray-200;
 }
 </style>
