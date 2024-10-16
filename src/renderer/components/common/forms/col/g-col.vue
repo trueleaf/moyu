fe2f8e03124dc40ac8c6f49056faa810b2487130
舒小凯 <2581105856@qq.com>
2024-10-16 21:58:56 +0800

    feat: 重构表单组件，优化代码结构和功能，支持动态插槽和参数绑定

--- src/renderer/components/common/forms/col/g-col.vue
+++ src/renderer/components/common/forms/col/g-col.vue
@@ -1,67 +1,41 @@
-/*
-    创建者：shuxiaokai
-    创建时间：2021-06-21 23:39
-    模块名称：el-row   el-col
-    备注：
-*/
+
 <template>
   <el-col :xs="24" :sm="24" :md="md" :lg="lg" :xl="xl">
     <slot />
   </el-col>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import { computed } from 'vue';
 
-export default defineComponent({
-  props: {
-    /**
-         * 是否一行显示
-         */
-    oneLine: {
-      type: Boolean,
-      default: false,
-    },
-    /**
-         * 是否只显示半行
-         */
-    halfLine: {
-      type: Boolean,
-      default: false,
-    },
+
+const props = defineProps({
+  oneLine: {
+    type: Boolean,
+    default: false,
   },
-  computed: {
-    md() { //处理oneline和halfLine
-      if (this.oneLine) {
-        return 24;
-      }
-      if (this.halfLine) {
-        return 12;
-      }
-      return 12;
-    },
-    lg() { //处理oneline和halfLine
-      if (this.oneLine) {
-        return 24;
-      }
-      if (this.halfLine) {
-        return 12;
-      }
-      return 6;
-    },
-    xl() { //处理oneline和halfLine
-      if (this.oneLine) {
-        return 24;
-      }
-      if (this.halfLine) {
-        return 12;
-      }
-      return 6;
-    },
+  halfLine: {
+    type: Boolean,
+    default: false,
   },
 })
+const md = computed(() => {
+  if (props.oneLine) {
+    return 24;
+  }
+  if (props.halfLine) {
+    return 12;
+  }
+  return 12;
+})
+const lg = computed(() => {
+  if (props.oneLine) {
+    return 24;
+  }
+})
+const xl = computed(() => {
+  if (props.oneLine) {
+    return 24;
+  }
+})
 </script>
-
-<style lang="scss" scoped>
-
-</style>
--- src/renderer/components/common/forms/form/g-form.vue
+++ src/renderer/components/common/forms/form/g-form.vue
@@ -15,119 +15,96 @@
   </el-form>
 </template>
 
-<script lang="ts">
-import { defineComponent, VNode } from 'vue'
-
+<script lang="ts" setup>
 import { config } from '@/../config/config'
 import initRules from './composables/rules'
 import { forEachForest, getTextWidth } from '@/helper';
 import { FormInstance } from 'element-plus';
+import { onMounted, provide, ref, useSlots, VNode, watch } from 'vue';
 
-export default defineComponent({
-  provide() {
-    return {
-      formInfo: this.formInfo, //将formInfo注入到item中
-    };
+const props = defineProps({
+  editData: {
+    type: Object,
+    default: () => ({})
   },
-  props: {
-    /**
-         * 表单数据
-         */
-    editData: {
-      type: Object,
-      default: () => ({})
-    },
-    /**
-         * 是否formInfo打印,生产环境不生效
-         */
-    showTips: {
-      type: Boolean,
-      default: false,
-    },
-    /**
-         * 展示rules,生产环境无效
-         */
-    showRules: {
-      type: Boolean,
-      default: false,
-    },
+  showTips: {
+    type: Boolean,
+    default: false,
   },
-  setup(_, context) {
-    const rules = initRules(context.slots);
-    return {
-      rules
-    };
+  showRules: {
+    type: Boolean,
+    default: false,
   },
-  data() {
-    return {
-      formInfo: {} as Record<string, unknown>, //---搜索参数
-      config, //------------------------------------配置信息
-      labelWidth: '100px', //-----------------------表单label宽度
-    };
-  },
-  watch: {
-    editData: {
-      handler(data) {
-        Object.keys(data).forEach((key) => {
-          this.formInfo[key] = data[key]
-        });
-      },
-      deep: true,
-      immediate: true,
-    },
-  },
-  mounted() {
-    this.initLabelWidth(); //初始化label的宽度
-    this.initFormData(); //初始化表单数据绑定
-  },
-  methods: {
-    //初始化label的宽度
-    initLabelWidth() {
-      const formItems: VNode[] = [];
-      if (this.$slots.default) {
-        const allSlots = this.$slots.default();
-        forEachForest<VNode>(allSlots, (slot: VNode) => {
-          const slotType = slot.type;
-          if (typeof slotType === 'object' && (slotType as Record<string, unknown>).name) {
-            formItems.push(slot);
-          }
-        })
+})
+
+const formInfo = ref<Record<string, unknown>>({});
+const labelWidth = ref<string>('100px');
+const form = ref<FormInstance>();
+const slots = useSlots();
+watch(() => props.editData, (data) => {
+  Object.keys(data).forEach((key) => {
+    formInfo.value[key] = data[key]
+  })
+}, { deep: true, immediate: true })
+provide('formInfo', formInfo)
+
+/*
+|--------------------------------------------------------------------------
+| 函数定义
+|--------------------------------------------------------------------------
+*/
+//初始化label的宽度
+const initLabelWidth = () => {
+  const formItems: VNode[] = [];
+  if (slots.default) {
+    const allSlots = slots.default();
+    forEachForest<VNode>(allSlots, (slot: VNode) => {
+      const slotType = slot.type;
+      if (typeof slotType === 'object' && (slotType as Record<string, unknown>).name) {
+        formItems.push(slot);
       }
-      const formDom: HTMLElement = this.$el;
-      const labelDom = formDom.querySelector('.el-form-item__label') || document.body;
-      const styleList = window.getComputedStyle(labelDom);
-      const { font } = styleList;
-      // eslint-disable-next-line prefer-spread
-      const maxLabelWidth = Math.max.apply(Math, formItems.map((val) => {
-        const { props } = val;
-        const label: string = props ? (props.label || '') : '';
-        const labelWidth = getTextWidth(label, font)
-        return labelWidth;
-      }));
-      const realWidth = maxLabelWidth < 100 ? 100 : maxLabelWidth;
-      this.labelWidth = `${Math.ceil(realWidth)}px`
-    },
-    //初始化表单参数
-    initFormData() {
-      if (this.$slots.default) {
-        const allSlots = this.$slots.default();
-        forEachForest<VNode>(allSlots, (slot: VNode) => {
-          const slotType = slot.type;
-          const { props } = slot;
-          if (typeof slotType === 'object' && (slotType as Record<string, unknown>).name) {
-            if (props && props.prop && !this.formInfo[props.prop]) {
-              this.formInfo[props.prop] = null;
-            }
-          }
-        })
+    })
+  }
+  const labelDom = form.value?.$el.querySelector('.el-form-item__label') || document.body;
+  const styleList = window.getComputedStyle(labelDom);
+  const { font } = styleList;
+  // eslint-disable-next-line prefer-spread
+  const maxLabelWidth = Math.max.apply(Math, formItems.map((val) => {
+    const { props } = val;
+    const label: string = props ? (props.label || '') : '';
+    const labelWidth = getTextWidth(label, font)
+    return labelWidth;
+  }));
+  const realWidth = maxLabelWidth < 100 ? 100 : maxLabelWidth;
+  labelWidth.value = `${Math.ceil(realWidth)}px`
+}
+//初始化表单参数
+const initFormData = () => {
+  if (slots.default) {
+    const allSlots = slots.default();
+    forEachForest<VNode>(allSlots, (slot: VNode) => {
+      const slotType = slot.type;
+      const { props } = slot;
+      if (typeof slotType === 'object' && (slotType as Record<string, unknown>).name) {
+        if (props && props.prop && !formInfo.value[props.prop]) {
+          formInfo.value[props.prop] = null;
+        }
       }
-    },
-    //手动校验
-    validate(fn: () => void) {
-      (this.$refs.form as FormInstance).validate(fn);
-    },
-  },
+    })
+  }
+}
+const validate = (fn: () => void) => {
+  form.value?.validate(fn);
+}
+const rules = initRules(slots);
+
+defineExpose({
+  validate,
+  formInfo
+})
+onMounted(() => {
+  initLabelWidth(); //初始化label的宽度
+  initFormData(); //初始化表单数据绑定
 })
 </script>
 
-<style lang="scss" scoped></style>
