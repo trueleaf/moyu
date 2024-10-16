a28e28e6bc030975321a0ece24e714fb35e0e696
舒小凯 <2581105856@qq.com>
2024-10-16 21:58:47 +0800

    refactor: 重构高亮和fieldset组件，优化代码结构和功能，支持动态插槽和样式

--- src/renderer/components/common/emphasize/g-emphasize.vue
+++ src/renderer/components/common/emphasize/g-emphasize.vue
@@ -1,9 +1,4 @@
-/*
-    创建者：shuxiaokai
-    创建时间：2021-07-29 22:36
-    模块名称：文字强调组件
-    备注：
-*/
+
 <template>
   <span>
     <span>{{ leftStr }}</span>
@@ -17,85 +12,57 @@
   </span>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import { computed, ref, watch } from 'vue';
 
-export default defineComponent({
-  props: {
-    /**
-         * 原始值
-         */
-    value: {
-      type: String,
-      default: '',
-    },
-    /**
-         * 关键字
-         */
-    keyword: {
-      type: String,
-      default: '',
-    },
-    /**
-         * 高亮颜色
-         */
-    activeColor: {
-      type: String,
-      default: '#f60',
-    },
-    /**
-         * 开启则背景颜色高亮而非文字高亮
-         */
-    background: {
-      type: Boolean,
-      default: false,
-    },
+const props = defineProps({
+  value: {
+    type: String,
+    default: '',
   },
-  data() {
-    return {
-      leftStr: '', //----------高亮字符串左侧字符串
-      emphasizeStr: '', //-----高亮字符串
-      rightStr: '', //---------高亮字符串右边字符串
-    };
+  keyword: {
+    type: String,
+    default: '',
   },
-  computed: {
-    isMatched(): boolean {
-      return !!(this.keyword && this.value.toLowerCase().match(this.keyword.toLowerCase()))
-    },
+  activeColor: {
+    type: String,
+    default: '#f60',
   },
-  watch: {
-    keyword: {
-      handler() {
-        this.spliceStr();
-      },
-      immediate: true,
-    },
-    value: {
-      handler() {
-        this.spliceStr();
-      },
-      immediate: true,
-    },
-  },
-  methods: {
-    spliceStr() {
-      const index = this.value.toLowerCase().indexOf(this.keyword.toLowerCase()); //匹配位置
-      const offset = this.keyword.length; //偏移位置
-      if (index === -1 || this.keyword.trim() === '') {
-        this.leftStr = this.value;
-        this.emphasizeStr = '';
-        this.rightStr = '';
-        return;
-      }
-      const strArr = this.value.split('');
-      this.leftStr = strArr.slice(0, index).join('');
-      this.emphasizeStr = strArr.slice(index, index + offset).join('');
-      this.rightStr = strArr.slice(index + offset).join('');
-    },
+  background: {
+    type: Boolean,
+    default: false,
   },
 })
+
+const leftStr = ref(''); //----------高亮字符串左侧字符串
+const emphasizeStr = ref(''); //-----高亮字符串
+const rightStr = ref(''); //---------高亮字符串右边字符串
+
+const isMatched = computed(() => {
+  return !!(props.keyword && props.value.toLowerCase().match(props.keyword.toLowerCase()));
+})
+const spliceStr = () => {
+  const index = props.value.toLowerCase().indexOf(props.keyword.toLowerCase()); //匹配位置
+  const offset = props.keyword.length; //偏移位置
+  if (index === -1 || props.keyword.trim() === '') {
+    leftStr.value = props.value;
+    emphasizeStr.value = '';
+    rightStr.value = '';
+    return;
+  }
+  const strArr = props.value.split('');
+  leftStr.value = strArr.slice(0, index).join('');
+  emphasizeStr.value = strArr.slice(index, index + offset).join('');
+  rightStr.value = strArr.slice(index + offset).join('');
+}
+watch(() => props.keyword, () => {
+  spliceStr();
+}, {
+  immediate: true,
+})
+watch(() => props.value, () => {
+  spliceStr();
+}, {
+  immediate: true,
+})
 </script>
-
-<style lang="scss" scoped>
-
-</style>
